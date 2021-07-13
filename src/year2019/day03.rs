22da37a753dbde13fcf90bfbe6c14aa25f6ed2b4
Dan Seminara <semi225599@gmail.com>
2021-07-13 19:31:13 -0400

    Check line segment intersections directly

--- src/year2019/day03.rs
+++ src/year2019/day03.rs
@@ -1,55 +1,106 @@
-use std::collections::HashMap;
-
 use crate::utils::Coord;
 
-fn parse_wires<'a>(input: &'a str) -> impl Iterator<Item = HashMap<Coord<i32>, i32>> + 'a {
-    input.lines().map(|line| {
-        let mut m = HashMap::new();
-        let mut pos = Coord::new(0, 0);
-        let mut steps = 0;
-        for p in line.split(',') {
-            let d = match &p[0..1] {
-                "U" => Coord::new(0, 1),
-                "D" => Coord::new(0, -1),
-                "L" => Coord::new(-1, 0),
-                "R" => Coord::new(1, 0),
-                _ => panic!("Unknown direction: {}", p),
-            };
-            for _ in 0..p[1..p.len()].parse().unwrap() {
-                pos += d;
-                steps += 1;
-                if !m.contains_key(&pos) {
-                    m.insert(pos, steps);
-                }
+#[derive(Eq, PartialEq)]
+enum Orientation {
+    V,
+    H,
+}
+
+struct Segment {
+    o: Orientation,
+    a: Coord<i32>,
+    b: Coord<i32>,
+    d: i32,
+    r: bool,
+}
+
+struct Wire {
+    parts: Vec<Segment>,
+}
+
+fn parse_wires(input: &str) -> Vec<Wire> {
+    input
+        .lines()
+        .map(|line| {
+            let mut pos = Coord::new(0, 0);
+            let mut steps = 0;
+            Wire {
+                parts: line
+                    .split(',')
+                    .map(|p| {
+                        let (o, d) = match &p[..1] {
+                            "U" => (Orientation::V, Coord::new(0, 1)),
+                            "D" => (Orientation::V, Coord::new(0, -1)),
+                            "L" => (Orientation::H, Coord::new(-1, 0)),
+                            "R" => (Orientation::H, Coord::new(1, 0)),
+                            _ => panic!("Unknown direction: {}", p),
+                        };
+                        let n = p[1..].parse().unwrap();
+                        let prev = pos;
+                        pos += d.scale(n);
+                        let (d, a, b, r) = if prev < pos {
+                            (steps, prev, pos, false)
+                        } else {
+                            (steps + n, pos, prev, true)
+                        };
+                        steps += n;
+                        Segment {
+                            o: o,
+                            a: a,
+                            b: b,
+                            d: d,
+                            r: r,
+                        }
+                    })
+                    .collect(),
             }
-        }
-        m
-    })
+        })
+        .collect()
+}
+
+impl Wire {
+    fn intersections<'a>(&'a self, other: &'a Self) -> impl Iterator<Item = (i32, i32)> + 'a {
+        self.parts.iter().flat_map(move |w1| {
+            other.parts.iter().filter_map(move |w2| {
+                if w1.o == w2.o {
+                    return None;
+                }
+                let (hs, vs) = if w1.o == Orientation::H {
+                    (w1, w2)
+                } else {
+                    (w2, w1)
+                };
+                (hs.a.x <= vs.a.x && vs.a.x <= hs.b.x && vs.a.y <= hs.a.y && hs.a.y <= vs.b.y).then(
+                    || {
+                        (
+                            vs.a.x.abs() + hs.a.y.abs(),
+                            hs.d + (if hs.r { -1 } else { 1 }) * (hs.a.x - vs.a.x).abs()
+                                + vs.d
+                                + (if vs.r { -1 } else { 1 }) * (vs.a.y - hs.a.y).abs(),
+                        )
+                    },
+                )
+            })
+        })
+    }
 }
 
 pub fn part1(input: &str) -> Option<i32> {
-    parse_wires(input)
-        .reduce(|mut a, b| {
-            a.retain(|k, _v| b.contains_key(&k));
-            a
+    let wires = &parse_wires(input);
+    (0..wires.len())
+        .flat_map(|w1| {
+            (w1 + 1..wires.len()).flat_map(move |w2| wires[w1].intersections(&wires[w2]))
         })
-        .unwrap()
-        .keys()
-        .map(|k| k.x.abs() + k.y.abs())
+        .map(|c| c.0)
         .min()
 }
 
 pub fn part2(input: &str) -> Option<i32> {
-    parse_wires(input)
-        .reduce(|mut a, b| {
-            a.retain(|k, _v| b.contains_key(&k));
-            for (k, v) in a.iter_mut() {
-                *v += b[&k];
-            }
-            a
+    let wires = &parse_wires(input);
+    (0..wires.len())
+        .flat_map(|w1| {
+            (w1 + 1..wires.len()).flat_map(move |w2| wires[w1].intersections(&wires[w2]))
         })
-        .unwrap()
-        .values()
-        .copied()
+        .map(|c| c.1)
         .min()
 }
