0e90c66c6719238d16b65953039ac2e40b1cd09a
Dan Seminara <semi225599@gmail.com>
2021-08-20 11:02:46 -0400

    Shorten parsing

--- src/year2020/day04.rs
+++ src/year2020/day04.rs
@@ -1,107 +1,72 @@
+use ahash::AHashSet;
 use nom::branch::alt;
-use nom::branch::permutation;
-use nom::bytes::complete::tag;
-use nom::bytes::complete::take_till;
-use nom::bytes::complete::take_while_m_n;
-use nom::character::complete::digit1;
-use nom::character::complete::multispace0;
-use nom::combinator::cond;
-use nom::combinator::opt;
+use nom::bytes::complete::{tag, take_till, take_while, take_while_m_n};
+use nom::character::complete::{digit1, multispace0};
 use nom::combinator::verify;
-use nom::map_res;
-use nom::named;
-use nom::recognize;
-use nom::sequence::pair;
-use nom::sequence::preceded;
-use nom::sequence::tuple;
+use nom::sequence::{pair, terminated};
+use nom::{map_res, named, recognize, IResult};
 use std::str::FromStr;
 
-named!(int <&str, i32>,
-       map_res!(recognize!(digit1), FromStr::from_str)
-);
+named!(int <&str, i32>, map_res!(recognize!(digit1), FromStr::from_str));
 
-fn make_parser<'a>(validate: bool) -> impl FnMut(&'a str) -> bool {
-    let byr = tuple((
-        tag("byr:"),
-        cond(validate, verify(int, |n| 1920 <= *n && *n <= 2002)),
-        cond(!validate, take_till(|c: char| c.is_ascii_whitespace())),
-    ));
-    let iyr = tuple((
-        tag("iyr:"),
-        cond(validate, verify(int, |n| 2010 <= *n && *n <= 2020)),
-        cond(!validate, take_till(|c: char| c.is_ascii_whitespace())),
-    ));
-    let eyr = tuple((
-        tag("eyr:"),
-        cond(validate, verify(int, |n| 2020 <= *n && *n <= 2030)),
-        cond(!validate, take_till(|c: char| c.is_ascii_whitespace())),
-    ));
-    let hgt = tuple((
-        tag("hgt:"),
-        cond(
-            validate,
-            verify(pair(int, alt((tag("cm"), tag("in")))), |(h, u)| match *u {
-                "cm" => 150 <= *h && *h <= 193,
-                "in" => 59 <= *h && *h <= 76,
-                _ => unreachable!(),
-            }),
-        ),
-        cond(!validate, take_till(|c: char| c.is_ascii_whitespace())),
-    ));
-    let hcl = tuple((
-        tag("hcl:"),
-        cond(
-            validate,
-            pair(
-                tag("#"),
-                take_while_m_n(6, 6, |c: char| c.is_ascii_hexdigit()),
-            ),
-        ),
-        cond(!validate, take_till(|c: char| c.is_ascii_whitespace())),
-    ));
-    let ecl = tuple((
-        tag("ecl:"),
-        cond(
-            validate,
-            alt((
-                tag("amb"),
-                tag("blu"),
-                tag("brn"),
-                tag("gry"),
-                tag("grn"),
-                tag("hzl"),
-                tag("oth"),
-            )),
-        ),
-        cond(!validate, take_till(|c: char| c.is_ascii_whitespace())),
-    ));
-    let pid = tuple((
-        tag("pid:"),
-        cond(validate, take_while_m_n(9, 9, |c: char| c.is_ascii_digit())),
-        cond(!validate, take_till(|c: char| c.is_ascii_whitespace())),
-    ));
-    let cid = pair(tag("cid:"), take_till(|c: char| c.is_ascii_whitespace()));
-    let mut parser = permutation((
-        preceded(multispace0, byr),
-        preceded(multispace0, iyr),
-        preceded(multispace0, eyr),
-        preceded(multispace0, hgt),
-        preceded(multispace0, hcl),
-        preceded(multispace0, ecl),
-        preceded(multispace0, pid),
-        opt(preceded(multispace0, cid)),
-    ));
-    move |s| parser(s).is_ok()
+fn parse(mut inp: &str, validate: bool) -> IResult<&str, ()> {
+    let mut req_fields: AHashSet<&str> = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+        .into_iter()
+        .collect();
+    while !req_fields.is_empty() {
+        let (i, field) = terminated(take_while(|c: char| c.is_ascii_alphabetic()), tag(":"))(inp)?;
+        let i = if validate {
+            match field {
+                "byr" => verify(int, |n| 1920 <= *n && *n <= 2002)(i)?.0,
+                "iyr" => verify(int, |n| 2010 <= *n && *n <= 2020)(i)?.0,
+                "eyr" => verify(int, |n| 2020 <= *n && *n <= 2030)(i)?.0,
+                "hgt" => {
+                    verify(pair(int, alt((tag("cm"), tag("in")))), |(h, u)| match *u {
+                        "cm" => 150 <= *h && *h <= 193,
+                        "in" => 59 <= *h && *h <= 76,
+                        _ => unreachable!(),
+                    })(i)?
+                    .0
+                }
+                "hcl" => {
+                    let i = tag("#")(i)?.0;
+                    take_while_m_n(6, 6, |c: char| c.is_ascii_hexdigit())(i)?.0
+                }
+                "ecl" => {
+                    alt((
+                        tag("amb"),
+                        tag("blu"),
+                        tag("brn"),
+                        tag("gry"),
+                        tag("grn"),
+                        tag("hzl"),
+                        tag("oth"),
+                    ))(i)?
+                    .0
+                }
+                "pid" => take_while_m_n(9, 9, |c: char| c.is_ascii_digit())(i)?.0,
+                _ => take_till(|c: char| c.is_ascii_whitespace())(i)?.0,
+            }
+        } else {
+            take_till(|c: char| c.is_ascii_whitespace())(i)?.0
+        };
+        inp = multispace0(i)?.0;
+        req_fields.remove(&field);
+    }
+    Ok((inp, ()))
 }
 
-fn count_matches<'a, F: FnMut(&'a str) -> bool>(mut parser: F, input: &'a str) -> usize {
-    input.split("\n\n").filter(|line| parser(line)).count()
+fn count_matches(input: &str, validate: bool) -> usize {
+    input
+        .split("\n\n")
+        .filter(|line| matches!(parse(line, validate), Ok(_)))
+        .count()
 }
 
 pub fn part1(input: &str) -> usize {
-    count_matches(make_parser(false), input)
+    count_matches(input, false)
 }
 
 pub fn part2(input: &str) -> usize {
-    count_matches(make_parser(true), input)
+    count_matches(input, true)
 }
