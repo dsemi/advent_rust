9fd3a612c2d032781c18ea8880653c7d074e6cfa
Dan Seminara <semi225599@gmail.com>
2021-08-19 21:16:19 -0400

    Speed up by removing regex and cache computed paths

--- src/year2020/day07.rs
+++ src/year2020/day07.rs
@@ -1,42 +1,53 @@
 use ahash::AHashMap;
-use regex::Regex;
 
-fn parse_bags(s: &str) -> AHashMap<String, Vec<(i64, String)>> {
-    let mut m = AHashMap::new();
-    let re = Regex::new(r"(?:(\d+) )?(\w+ \w+) bags?").unwrap();
-    for line in s.lines() {
-        let bags = re.captures_iter(line).collect::<Vec<_>>();
-        m.insert(
-            bags[0][2].to_string(),
-            bags[1..]
-                .iter()
-                .filter(|x| &x[2] != "no other")
-                .map(|x| (x[1].parse().unwrap(), x[2].to_string()))
-                .collect(),
-        );
-    }
-    m
+fn parse_bags(s: &str) -> AHashMap<&str, Vec<(u32, &str)>> {
+    s.lines()
+        .map(|line| {
+            let (outer_bag, inner_bags) = line[..line.len() - 1].split_once(" contain ").unwrap();
+            (
+                outer_bag.rsplit_once(' ').unwrap().0,
+                inner_bags
+                    .split(", ")
+                    .filter(|&bag| bag != "no other bags")
+                    .map(|bag| {
+                        let (n, name) = bag.rsplit_once(' ').unwrap().0.split_once(' ').unwrap();
+                        (n.parse().unwrap(), name)
+                    })
+                    .collect(),
+            )
+        })
+        .collect()
 }
 
-fn holds_shiny_gold(m: &AHashMap<String, Vec<(i64, String)>>, k: &str) -> bool {
-    m[k]
-        .iter()
-        .any(|(_, k2)| k2 == "shiny gold" || holds_shiny_gold(m, k2))
+fn holds_shiny_gold<'a>(
+    cache: &mut AHashMap<&'a str, bool>,
+    m: &AHashMap<&str, Vec<(u32, &'a str)>>,
+    k: &'a str,
+) -> bool {
+    match cache.get(k) {
+        None => {
+            let v = m[k]
+                .iter()
+                .any(|(_, k2)| k2 == &"shiny gold" || holds_shiny_gold(cache, m, k2));
+            cache.insert(k, v);
+            v
+        }
+        Some(v) => *v,
+    }
 }
 
 pub fn part1(input: &str) -> usize {
     let m = parse_bags(input);
-    m.keys().filter(|&k| holds_shiny_gold(&m, k)).count()
+    let mut c = AHashMap::new();
+    m.keys()
+        .filter(|&k| holds_shiny_gold(&mut c, &m, k))
+        .count()
 }
 
-fn count_bags(m: &AHashMap<String, Vec<(i64, String)>>, k: &str) -> i64 {
-    m[k]
-        .iter()
-        .map(|(n, k2)| n + n * count_bags(m, k2))
-        .sum()
+fn count_bags(m: &AHashMap<&str, Vec<(u32, &str)>>, k: &str) -> u32 {
+    m[k].iter().map(|(n, k2)| n + n * count_bags(m, k2)).sum()
 }
 
-pub fn part2(input: &str) -> i64 {
-    let m = parse_bags(input);
-    count_bags(&m, "shiny gold")
+pub fn part2(input: &str) -> u32 {
+    count_bags(&parse_bags(input), "shiny gold")
 }
