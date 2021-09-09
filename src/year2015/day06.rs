941d095c02213961fcc6bc4f194d119610a3760c
Dan Seminara <semi225599@gmail.com>
2021-09-09 01:47:25 -0400

    Pass slice to functions to improve perf

--- src/year2015/day06.rs
+++ src/year2015/day06.rs
@@ -1,11 +1,9 @@
-use std::cmp::max;
-
 fn run_commands(
     input: &str,
-    off: fn(i32) -> i32,
-    on: fn(i32) -> i32,
-    toggle: fn(i32) -> i32,
-) -> i32 {
+    off: fn(&mut [u32]),
+    on: fn(&mut [u32]),
+    toggle: fn(&mut [u32]),
+) -> u32 {
     let mut grid = vec![0; 1000000];
     for line in input.lines() {
         let (cmdstr, x0, y0, x1, y1) = scan_fmt!(
@@ -26,19 +24,30 @@ fn run_commands(
         };
 
         for x in x0..=x1 {
-            for y in y0..=y1 {
-                let v = &mut grid[1000 * x + y];
-                *v = f(*v);
-            }
+            let row = 1000 * x;
+            f(&mut grid[row + y0..=row + y1]);
         }
     }
     grid.into_iter().sum()
 }
 
-pub fn part1(input: &str) -> i32 {
-    run_commands(input, |_| 0, |_| 1, |x| x ^ 1)
+pub fn part1(input: &str) -> u32 {
+    run_commands(
+        input,
+        |s| s.fill(0),
+        |s| s.fill(1),
+        |s| s.iter_mut().for_each(|v| *v ^= 1),
+    )
 }
 
-pub fn part2(input: &str) -> i32 {
-    run_commands(input, |x| max(0, x - 1), |x| x + 1, |x| x + 2)
+pub fn part2(input: &str) -> u32 {
+    run_commands(
+        input,
+        |s| {
+            s.iter_mut()
+                .for_each(|v| *v = v.checked_sub(1).unwrap_or(0))
+        },
+        |s| s.iter_mut().for_each(|v| *v += 1),
+        |s| s.iter_mut().for_each(|v| *v += 2),
+    )
 }
