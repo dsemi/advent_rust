7b28b87b995de5f32313d9be4f23e998caf733e9
Dan Seminara <semi225599@gmail.com>
2021-09-10 11:50:55 -0400

    Don't create vec when it's not needed

--- src/year2015/day24.rs
+++ src/year2015/day24.rs
@@ -1,26 +1,26 @@
 use itertools::Itertools;
 
-fn quantum_entanglement(n: i64, s: &str) -> Option<i64> {
+fn quantum_entanglement(n: i64, s: &str) -> i64 {
     let wts: Vec<i64> = s.lines().map(|x| x.parse().unwrap()).collect();
     let group_size: i64 = wts.iter().copied().sum::<i64>() / n;
     let mut i = 1;
     loop {
-        let qes: Vec<i64> = wts.iter()
+        let min_qe = wts.iter()
             .combinations(i)
             .filter(|combo| combo.iter().copied().sum::<i64>() == group_size)
             .map(|combo| combo.into_iter().product())
-            .collect();
-        if !qes.is_empty() {
-            return qes.into_iter().min();
+            .min();
+        if let Some(m) = min_qe {
+            return m;
         }
         i += 1;
     }
 }
 
-pub fn part1(input: &str) -> Option<i64> {
+pub fn part1(input: &str) -> i64 {
     quantum_entanglement(3, input)
 }
 
-pub fn part2(input: &str) -> Option<i64> {
+pub fn part2(input: &str) -> i64 {
     quantum_entanglement(4, input)
 }
