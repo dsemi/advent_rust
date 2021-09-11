88ac3150cc43b52e00dc2876142e4f4de7e775b9
Dan Seminara <semi225599@gmail.com>
2021-09-11 14:06:32 -0400

    Preprocess pipes to be in the same buckets

--- src/year2017/day24.rs
+++ src/year2017/day24.rs
@@ -1,56 +1,66 @@
 use std::cmp::max;
 
 struct Pipe {
+    id: usize,
     a: u32,
     b: u32,
-    used: bool,
 }
 
 fn parse_pipes(input: &str) -> Vec<Pipe> {
     input
         .lines()
-        .map(|line| {
+        .enumerate()
+        .map(|(id, line)| {
             let (a, b) = line.split_once('/').unwrap();
             Pipe {
+                id,
                 a: a.parse().unwrap(),
                 b: b.parse().unwrap(),
-                used: false,
             }
         })
         .collect()
 }
 
-fn solve<T: Copy + Ord>(input: &str, mut start: T, step: fn(T, &Pipe) -> T) -> T {
-    let mut pipes = parse_pipes(input);
-    fn dfs<T: Copy + Ord>(
-        pipes: &mut Vec<Pipe>,
-        start: &mut T,
-        step: fn(T, &Pipe) -> T,
-        pins: u32,
-        v: T,
-    ) {
-        *start = max(*start, v);
-        for i in 0..pipes.len() {
-            if !pipes[i].used && (pipes[i].a == pins || pipes[i].b == pins) {
-                pipes[i].used = true;
-                dfs(
-                    pipes,
-                    start,
-                    step,
-                    if pipes[i].a == pins {
-                        pipes[i].b
-                    } else {
-                        pipes[i].a
-                    },
-                    step(v, &pipes[i]),
-                );
-                pipes[i].used = false;
-            }
+fn build<T: Copy + Ord>(
+    used: &mut Vec<bool>,
+    a_s: &Vec<Vec<&Pipe>>,
+    b_s: &Vec<Vec<&Pipe>>,
+    step: fn(T, &Pipe) -> T,
+    port: u32,
+    curr: T,
+) -> T {
+    let mut mx = curr;
+    for pipe in a_s[port as usize].iter() {
+        if !used[pipe.id] {
+            used[pipe.id] = true;
+            mx = max(mx, build(used, a_s, b_s, step, pipe.b, step(curr, pipe)));
+            used[pipe.id] = false;
         }
     }
-    let v = start;
-    dfs(&mut pipes, &mut start, step, 0, v);
-    start
+
+    for pipe in b_s[port as usize].iter() {
+        if !used[pipe.id] {
+            used[pipe.id] = true;
+            mx = max(mx, build(used, a_s, b_s, step, pipe.a, step(curr, pipe)));
+            used[pipe.id] = false;
+        }
+    }
+    mx
+}
+
+fn solve<T: Copy + Ord>(input: &str, start: T, step: fn(T, &Pipe) -> T) -> T {
+    let pipes = parse_pipes(input);
+    let mut used = vec![false; pipes.len()];
+    let mx = pipes.iter().flat_map(|p| vec![p.a, p.b]).max().unwrap() as usize;
+    let mut a_s: Vec<Vec<&Pipe>> = vec![vec![]; mx + 1];
+    let mut b_s: Vec<Vec<&Pipe>> = vec![vec![]; mx + 1];
+    for pipe in pipes.iter() {
+        a_s[pipe.a as usize].push(pipe);
+        if pipe.a != pipe.b {
+            b_s[pipe.b as usize].push(pipe);
+        }
+    }
+    build(&mut used, &a_s, &b_s, step, 0, start)
 }
 
 pub fn part1(input: &str) -> u32 {
