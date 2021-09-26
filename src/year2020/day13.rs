8249d98d07df3a640e3d97c99e7fae8264396a2a
Dan Seminara <semi225599@gmail.com>
2021-09-26 18:25:51 -0400

    Use chinese_remainder in utils rather than redefining

--- src/year2020/day13.rs
+++ src/year2020/day13.rs
@@ -1,33 +1,4 @@
-fn chinese_remainder(an: Vec<(i64, i64)>) -> i64 {
-    let prod: i64 = an.iter().map(|(_, x)| x).product();
-    let mut sum = 0;
-    for (a_i, n_i) in an {
-        let p = prod / n_i;
-        sum += a_i * mul_inv(p, n_i) * p;
-        sum = sum.rem_euclid(prod);
-    }
-    sum
-}
-
-fn mul_inv(a: i64, b: i64) -> i64 {
-    let mut ab = (a, b);
-    let b0 = b;
-    let mut x0x1 = (0, 1);
-    if b == 1 {
-        return 1;
-    }
-    while ab.0 > 1 {
-        let (a, b) = ab;
-        let q = a / b;
-        ab = (b, a % b);
-        let (x0, x1) = x0x1;
-        x0x1 = (x1 - q * x0, x0);
-    }
-    if x0x1.1 < 0 {
-        x0x1.1 += b0;
-    }
-    x0x1.1
-}
+use crate::utils::chinese_remainder;
 
 fn parse_buses(s: &str) -> (i64, Vec<(i64, i64)>) {
     let ls: Vec<&str> = s.lines().collect();
