4adfbd5ff56df95212588853cb60c5f63204777c
Dan Seminara <semi225599@gmail.com>
2021-10-07 17:54:05 -0400

    Restructure angle calculations

--- src/year2019/day10.rs
+++ src/year2019/day10.rs
@@ -1,7 +1,7 @@
-use ahash::AHashMap;
 use gcd::Gcd;
 use std::cmp::Ordering;
 use std::cmp::Ordering::*;
+use std::collections::BTreeMap;
 
 type Coord = (i32, i32);
 
@@ -19,46 +19,61 @@ fn parse_coords(input: &str) -> Vec<Coord> {
         .collect()
 }
 
-fn dist(a: Coord, b: Coord) -> i32 {
+fn dist(a: &Coord, b: &Coord) -> i32 {
     (b.0 - a.0).abs() + (b.1 - a.1).abs()
 }
 
-fn theta(a: Coord, b: Coord) -> Coord {
-    let (x, y) = (b.0 - a.0, b.1 - a.1);
-    let gcd = (x.abs() as u32).gcd(y.abs() as u32) as i32;
-    (x / gcd, y / gcd)
+#[derive(Eq, PartialEq)]
+struct Angle {
+    x: i32,
+    y: i32,
 }
 
-fn cmp(a: &Coord, b: &Coord) -> Ordering {
-    if a.0 >= 0 && b.0 < 0 {
-        return Less;
-    } else if a.0 < 0 && b.0 >= 0 {
-        return Greater;
-    } else if a.0 == 0 && b.0 == 0 {
-        return a.1.cmp(&b.1);
+impl Angle {
+    fn new(a: &Coord, b: &Coord) -> Self {
+        let (x, y) = (b.0 - a.0, b.1 - a.1);
+        let gcd = (x.abs() as u32).gcd(y.abs() as u32) as i32;
+        Self {
+            x: x / gcd,
+            y: y / gcd,
+        }
     }
+}
 
-    let det = a.0 * (-b.1) - (-a.1) * b.0;
-    det.cmp(&0)
+impl PartialOrd for Angle {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Angle {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.x >= 0 && other.x < 0 {
+            Less
+        } else if self.x < 0 && other.x >= 0 {
+            Greater
+        } else if self.x == 0 && other.x == 0 {
+            self.y.cmp(&other.y)
+        } else {
+            let det = self.x * (-other.y) - (-self.y) * other.x;
+            det.cmp(&0)
+        }
+    }
 }
 
 fn visibilities(pt: &Coord, pts: &[Coord]) -> Vec<Vec<Coord>> {
-    let mut m: AHashMap<Coord, Vec<Coord>> = AHashMap::new();
+    let mut m: BTreeMap<Angle, Vec<Coord>> = BTreeMap::new();
     for p in pts.iter() {
         if p != pt {
-            let e = m.entry(theta(*pt, *p)).or_insert_with(Vec::new);
-            (*e).push(*p);
+            let e = m.entry(Angle::new(pt, p)).or_insert_with(Vec::new);
+            let idx = match e.binary_search_by_key(&dist(pt, p), |x| dist(pt, x)) {
+                Ok(i) => i,
+                Err(i) => i,
+            };
+            e.insert(idx, *p);
         }
     }
-    let mut vec: Vec<Coord> = m.keys().copied().collect();
-    vec.sort_by(cmp);
-    vec.into_iter()
-        .map(|k| {
-            let mut v = m[&k].clone();
-            v.sort_by_key(|x| dist(*pt, *x));
-            v
-        })
-        .collect()
+    m.into_values().collect()
 }
 
 fn max_detected(asts: Vec<Coord>) -> Vec<Vec<Coord>> {
@@ -72,23 +87,11 @@ pub fn part1(input: &str) -> usize {
     max_detected(parse_coords(input)).len()
 }
 
-pub fn part2(input: &str) -> i32 {
-    let mut m: Vec<std::vec::IntoIter<Coord>> = max_detected(parse_coords(input))
+pub fn part2(input: &str) -> Option<i32> {
+    max_detected(parse_coords(input))
         .into_iter()
         .map(|x| x.into_iter())
-        .collect();
-    let mut i = 0;
-    loop {
-        for pts in m.iter_mut() {
-            match pts.next() {
-                Some((a, b)) => {
-                    i += 1;
-                    if i == 200 {
-                        return 100 * a + b;
-                    }
-                }
-                None => continue,
-            }
-        }
-    }
+        .cycle()
+        .filter_map(|mut pts| pts.next().map(|(a, b)| 100 * a + b))
+        .nth(199)
 }
