ef1818154a5b6736ad48d291715b95601f4babaa
Dan Seminara <semi225599@gmail.com>
2021-10-22 11:51:52 -0400

    Move used to Pipe field

--- src/year2017/day24.rs
+++ src/year2017/day24.rs
@@ -2,28 +2,26 @@ use std::cell::RefCell;
 use std::cmp::max;
 
 struct Pipe {
-    id: usize,
     a: u32,
     b: u32,
+    used: RefCell<bool>,
 }
 
 fn parse_pipes(input: &str) -> Vec<Pipe> {
     input
         .lines()
-        .enumerate()
-        .map(|(id, line)| {
+        .map(|line| {
             let (a, b) = line.split_once('/').unwrap();
             Pipe {
-                id,
                 a: a.parse().unwrap(),
                 b: b.parse().unwrap(),
+                used: RefCell::new(false),
             }
         })
         .collect()
 }
 
 struct Builder<'a, T> {
-    used: RefCell<Vec<bool>>,
     a_s: Vec<Vec<&'a Pipe>>,
     b_s: Vec<Vec<&'a Pipe>>,
     f: fn(T, &Pipe) -> T,
@@ -34,10 +32,10 @@ impl<'a, T: Copy + Ord> Builder<'a, T> {
         let mut m = curr;
         for vec in [&self.a_s, &self.b_s] {
             for pipe in vec[port as usize].iter() {
-                if !self.used.borrow()[pipe.id] {
-                    self.used.borrow_mut()[pipe.id] = true;
+                if !*pipe.used.borrow() {
+                    *pipe.used.borrow_mut() = true;
                     m = max(m, self.build(pipe.a + pipe.b - port, (self.f)(curr, pipe)));
-                    self.used.borrow_mut()[pipe.id] = false;
+                    *pipe.used.borrow_mut() = false;
                 }
             }
         }
@@ -47,7 +45,6 @@ impl<'a, T: Copy + Ord> Builder<'a, T> {
 
 fn solve<T: Copy + Ord>(input: &str, start: T, step: fn(T, &Pipe) -> T) -> T {
     let pipes = parse_pipes(input);
-    let used = vec![false; pipes.len()];
     let mx = pipes.iter().flat_map(|p| vec![p.a, p.b]).max().unwrap() as usize;
     let mut a_s: Vec<Vec<&Pipe>> = vec![vec![]; mx + 1];
     let mut b_s: Vec<Vec<&Pipe>> = vec![vec![]; mx + 1];
@@ -57,13 +54,7 @@ fn solve<T: Copy + Ord>(input: &str, start: T, step: fn(T, &Pipe) -> T) -> T {
             b_s[pipe.b as usize].push(pipe);
         }
     }
-    Builder {
-        a_s,
-        b_s,
-        used: RefCell::new(used),
-        f: step,
-    }
-    .build(0, start)
+    Builder { a_s, b_s, f: step }.build(0, start)
 }
 
 pub fn part1(input: &str) -> u32 {
