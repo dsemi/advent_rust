c6e5db3c9aaa77367ab86766421c175b3f3fad9f
Dan Seminara <semi225599@gmail.com>
2021-11-02 02:20:29 -0400

    Add back real parsing

--- src/year2017/day25.rs
+++ src/year2017/day25.rs
@@ -1,74 +1,78 @@
 use std::collections::VecDeque;
 
-use crate::year2017::day25::Dir::L;
-use crate::year2017::day25::Dir::R;
-use crate::year2017::day25::State::A;
-use crate::year2017::day25::State::B;
-use crate::year2017::day25::State::C;
-use crate::year2017::day25::State::D;
-use crate::year2017::day25::State::E;
-use crate::year2017::day25::State::F;
-
-#[derive(Clone, Copy)]
-enum State {
-    A, B, C, D, E, F
-}
-
-#[derive(Clone, Copy)]
 enum Dir {
-    L, R
-}
-
-fn go(d: Dir, tape: &mut VecDeque<i64>, i: &mut usize) {
-    match d {
-        L => if *i == 0 {
-            tape.push_front(0);
-        } else {
-            *i -= 1;
-        },
-        R => {
-            *i += 1;
-            if *i >= tape.len() {
-                tape.push_back(0);
-            }
-        },
-    }
+    L,
+    R,
 }
 
 struct Rule {
-    write: i64,
+    write: usize,
     dir: Dir,
-    state: State,
+    state: usize,
 }
 
-fn make_rules() -> [[Rule; 2]; 6] {
-    [
-        [Rule { write: 1, dir: R, state: B }, Rule { write: 0, dir: L, state: F }],
-        [Rule { write: 0, dir: R, state: C }, Rule { write: 0, dir: R, state: D }],
-        [Rule { write: 1, dir: L, state: D }, Rule { write: 1, dir: R, state: E }],
-        [Rule { write: 0, dir: L, state: E }, Rule { write: 0, dir: L, state: D }],
-        [Rule { write: 0, dir: R, state: A }, Rule { write: 1, dir: R, state: C }],
-        [Rule { write: 1, dir: L, state: A }, Rule { write: 1, dir: R, state: A }],
-    ]
+fn parse_branch<'a, I: Iterator<Item = &'a str>>(gen: &mut I, idx: usize) -> Option<Rule> {
+    assert!(gen.next()? == format!("  If the current value is {}:", idx));
+    let write = scan_fmt!(gen.next()?, "    - Write the value {}.", usize).unwrap();
+    let dir = scan_fmt!(gen.next()?, "    - Move one slot to the {/left|right/}.", String).unwrap();
+    let state = scan_fmt!(gen.next()?, "    - Continue with state {}.", char).unwrap();
+    Some(Rule {
+        write,
+        dir: if dir == "left" { Dir::L } else { Dir::R },
+        state: state as usize - 'A' as usize,
+    })
 }
 
-fn process(steps: usize) -> i64 {
+fn parse_state(input: &str) -> Option<[Rule; 2]> {
+    let mut gen = input.lines();
+    scan_fmt!(gen.next()?, "In state {}:", char).unwrap();
+    let rule1 = parse_branch(&mut gen, 0)?;
+    let rule2 = parse_branch(&mut gen, 1)?;
+    Some([rule1, rule2])
+}
+
+fn parse_rules(input: &str) -> (usize, usize, Vec<[Rule; 2]>) {
+    let mut gen = input.split("\n\n");
+    let (start, n) = scan_fmt!(
+        gen.next().unwrap(),
+        "Begin in state {}.\nPerform a diagnostic checksum after {} steps.",
+        char,
+        usize
+    )
+    .unwrap();
+    (
+        start as usize - 'A' as usize,
+        n,
+        gen.map(|st| parse_state(st).unwrap()).collect(),
+    )
+}
+
+pub fn part1(input: &str) -> usize {
+    let (mut state, steps, rules) = parse_rules(input);
     let mut tape = VecDeque::new();
     tape.push_back(0);
     let mut i = 0;
-    let mut state = A;
-    let rules = make_rules();
     for _ in 0..steps {
-        let rule = &rules[state as usize][tape[i] as usize];
+        let rule = &rules[state as usize][tape[i]];
         tape[i] = rule.write;
-        go(rule.dir, &mut tape, &mut i);
+        match rule.dir {
+            Dir::L => {
+                if i == 0 {
+                    tape.push_front(0);
+                } else {
+                    i -= 1;
+                }
+            }
+            Dir::R => {
+                i += 1;
+                if i >= tape.len() {
+                    tape.push_back(0);
+                }
+            }
+        }
         state = rule.state;
     }
     tape.into_iter().sum()
-}
-
-pub fn part1(_input: &str) -> i64 {
-    process(12794428)
 }
 
 pub fn part2(_input: &str) -> String {
