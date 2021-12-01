8bb170691bebf9fe7a70e2b2e6eb8d07b535fda3
Dan Seminara <semi225599@gmail.com>
2021-12-01 15:06:18 -0500

    Use zip rather than indexing

--- src/year2021/day01.rs
+++ src/year2021/day01.rs
@@ -1,5 +1,5 @@
 fn solve(ns: Vec<i32>, off: usize) -> usize {
-    (0..ns.len() - off).filter(|&i| ns[i] < ns[i + off]).count()
+    ns.iter().zip(&ns[off..]).filter(|(a, b)| a < b).count()
 }
 
 pub fn part1(input: Vec<i32>) -> usize {
