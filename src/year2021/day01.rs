b97056e8e33569afa345493c97b5b3399e08e471
Dan Seminara <semi225599@gmail.com>
2021-12-01 00:34:08 -0500

    2021 Day 1

---
+++ src/year2021/day01.rs
@@ -0,0 +1,21 @@
+fn count_decreasing<I: Iterator<Item = i32>>(ns: I) -> usize {
+    ns.collect::<Vec<i32>>()
+        .windows(2)
+        .filter(|w| w[0] < w[1])
+        .count()
+}
+
+pub fn part1(input: &str) -> usize {
+    count_decreasing(input.lines().map(|x| x.parse().unwrap()))
+}
+
+pub fn part2(input: &str) -> usize {
+    count_decreasing(
+        input
+            .lines()
+            .map(|x| x.parse().unwrap())
+            .collect::<Vec<i32>>()
+            .windows(3)
+            .map(|w| w.iter().sum()),
+    )
+}
