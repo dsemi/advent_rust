3e8654b7a594ad9ba33bd21be4d472f34b2f116d
Dan Seminara <semi225599@gmail.com>
2021-12-02 00:32:45 -0500

    Return tuple rather than passing in mutable refs

--- src/year2021/day02.rs
+++ src/year2021/day02.rs
@@ -1,27 +1,27 @@
-fn run(input: &str, horz: &mut usize, depth: &mut usize, aim: &mut usize) {
+fn run(input: &str) -> (i32, i32, i32) {
+    let (mut horz, mut depth, mut aim) = (0, 0, 0);
     for line in input.lines() {
         let (cmd, ns) = line.split_once(' ').unwrap();
-        let n: usize = ns.parse().unwrap();
+        let n: i32 = ns.parse().unwrap();
         match cmd {
             "forward" => {
-                *horz += n;
-                *depth += *aim * n
+                horz += n;
+                depth += aim * n
             }
-            "down" => *aim += n,
-            "up" => *aim -= n,
+            "down" => aim += n,
+            "up" => aim -= n,
             _ => panic!("Bad input"),
         }
     }
+    (horz, depth, aim)
 }
 
-pub fn part1(input: &str) -> usize {
-    let (mut horz, mut depth, mut aim) = (0, 0, 0);
-    run(input, &mut horz, &mut aim, &mut depth);
+pub fn part1(input: &str) -> i32 {
+    let (horz, _, depth) = run(input);
     horz * depth
 }
 
-pub fn part2(input: &str) -> usize {
-    let (mut horz, mut depth, mut aim) = (0, 0, 0);
-    run(input, &mut horz, &mut depth, &mut aim);
+pub fn part2(input: &str) -> i32 {
+    let (horz, depth, _) = run(input);
     horz * depth
 }
