ba9441c571b941c6a71337d24ca1c8243152cd16
Dan Seminara <semi225599@gmail.com>
2021-12-04 02:58:58 -0500

    Add back build script to catch newly added days

---
+++ build.rs
@@ -0,0 +1,16 @@
+use walkdir::WalkDir;
+
+fn main() {
+    for entry in WalkDir::new("src") {
+        let ent = entry.unwrap();
+        let path = ent.path();
+        if let Some(name) = path.file_name() {
+            if let Some(s) = name.to_str() {
+                if !s.starts_with("#") && !s.starts_with(".#") && s.ends_with(".rs") {
+                    println!("cargo:rerun-if-changed={}", path.display());
+                }
+            }
+        }
+    }
+    println!("cargo:rerun-if-changed=build.rs");
+}
