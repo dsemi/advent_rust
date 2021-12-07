4f7a2c2cb8f1ffa6e6e30cd67941b953d56ef50f
Dan Seminara <semi225599@gmail.com>
2021-12-07 13:01:02 -0500

    2021 Day 7

---
+++ src/year2021/day07.rs
@@ -0,0 +1,27 @@
+use std::cmp::min;
+
+pub fn part1(input: &str) -> i32 {
+    let mut ns: Vec<i32> = input.split(',').map(|n| n.parse().unwrap()).collect();
+    ns.sort_unstable();
+    let med = if ns.len() % 2 == 0 {
+        (ns[ns.len() / 2 - 1] + ns[ns.len() / 2]) / 2
+    } else {
+        ns[ns.len() / 2]
+    };
+    ns.into_iter().map(|n| (n - med).abs()).sum()
+}
+
+fn tri(n: i32) -> i32 {
+    n * (n + 1) / 2
+}
+
+pub fn part2(input: &str) -> i32 {
+    let ns: Vec<i32> = input.split(',').map(|n| n.parse().unwrap()).collect();
+    let mean = (ns.iter().sum::<i32>() as f32) / ns.len() as f32;
+    min(
+        ns.iter()
+            .map(|n| tri((n - mean.floor() as i32).abs()))
+            .sum(),
+        ns.iter().map(|n| tri((n - mean.ceil() as i32).abs())).sum(),
+    )
+}
