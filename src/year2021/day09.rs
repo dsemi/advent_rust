9baedb08c11cd79be41857ac71732b6f554b5583
Dan Seminara <semi225599@gmail.com>
2021-12-09 12:40:12 -0500

    Shorten

--- src/year2021/day09.rs
+++ src/year2021/day09.rs
@@ -3,7 +3,7 @@ use ahash::AHashSet;
 
 type Coord = (usize, usize);
 
-fn neighbs<'a>(grid: &'a [Vec<u32>], c: &(usize, usize)) -> impl Iterator<Item = Coord> + 'a {
+fn neighbs<'a>(grid: &'a [Vec<u32>], c: &Coord) -> impl Iterator<Item = Coord> + 'a {
     [
         (c.0 - 1, c.1),
         (c.0 + 1, c.1),
@@ -11,7 +11,17 @@ fn neighbs<'a>(grid: &'a [Vec<u32>], c: &(usize, usize)) -> impl Iterator<Item =
         (c.0, c.1 + 1),
     ]
     .into_iter()
-    .filter_map(|p| grid.get(p.0)?.get(p.1).is_some().then(|| p))
+    .filter_map(|p| grid.get(p.0)?.get(p.1).map(|_| p))
+}
+
+fn lows(grid: &[Vec<u32>]) -> impl Iterator<Item = Coord> + '_ {
+    (0..grid.len()).flat_map(move |r| {
+        (0..grid[r].len()).filter_map(move |c| {
+            neighbs(grid, &(r, c))
+                .all(|p| grid[r][c] < grid[p.0][p.1])
+                .then(|| (r, c))
+        })
+    })
 }
 
 pub fn part1(input: &str) -> u32 {
@@ -19,15 +29,7 @@ pub fn part1(input: &str) -> u32 {
         .lines()
         .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
-    let mut risk = 0;
-    for r in 0..grid.len() {
-        for c in 0..grid[r].len() {
-            if neighbs(&grid, &(r, c)).all(|p| grid[r][c] < grid[p.0][p.1]) {
-                risk += 1 + grid[r][c]
-            }
-        }
-    }
-    risk
+    lows(&grid).map(|(r, c)| 1 + grid[r][c]).sum()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -36,19 +38,13 @@ pub fn part2(input: &str) -> usize {
         .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
     let mut visited = AHashSet::new();
-    let mut basins = vec![];
-    for r in 0..grid.len() {
-        for c in 0..grid[r].len() {
-            let pt = (r, c);
-            if !visited.contains(&pt) && grid[r][c] != 9 {
-                basins.push(
-                    bfs(pt, |p| neighbs(&grid, p).filter(|&(r, c)| grid[r][c] != 9))
-                        .map(|(_, p)| visited.insert(p))
-                        .count(),
-                );
-            }
-        }
-    }
+    let mut basins = lows(&grid)
+        .map(|pt| {
+            bfs(pt, |p| neighbs(&grid, p).filter(|&(r, c)| grid[r][c] != 9))
+                .take_while(|(_, p)| visited.insert(*p))
+                .count()
+        })
+        .collect::<Vec<_>>();
     basins.sort_unstable();
     basins[basins.len() - 3..].iter().product()
 }
