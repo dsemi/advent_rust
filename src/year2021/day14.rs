acf066a26920798f46bc069c5ff0b002a2e31e24
Dan Seminara <semi225599@gmail.com>
2021-12-14 01:03:45 -0500

    2021 Day 14

---
+++ src/year2021/day14.rs
@@ -0,0 +1,37 @@
+use ahash::AHashMap;
+
+fn polymerize(input: &str, n: usize) -> usize {
+    let (tmpl, rest) = input.split_once("\n\n").unwrap();
+    let vtmpl = tmpl.chars().collect::<Vec<_>>();
+    let d: AHashMap<Vec<char>, char> = rest
+        .lines()
+        .map(|line| line.split_once(" -> ").unwrap())
+        .map(|(a, b)| (a.chars().collect(), b.chars().next().unwrap()))
+        .collect();
+    let mut cnts = AHashMap::new();
+    for k in vtmpl.windows(2) {
+        *cnts.entry(k.to_owned()).or_insert(0) += 1;
+    }
+    for _ in 0..n {
+        let mut cnts2 = AHashMap::new();
+        for (k, v) in cnts {
+            let rep = d[&k];
+            *cnts2.entry(vec![k[0], rep]).or_insert(0) += v;
+            *cnts2.entry(vec![rep, k[1]]).or_insert(0) += v;
+        }
+        cnts = cnts2;
+    }
+    let mut lets = AHashMap::new();
+    cnts.into_iter()
+        .for_each(|(k, v)| *lets.entry(k[0]).or_insert(0) += v);
+    *lets.entry(*vtmpl.last().unwrap()).or_insert(0) += 1;
+    lets.values().max().unwrap() - lets.values().min().unwrap()
+}
+
+pub fn part1(input: &str) -> usize {
+    polymerize(input, 10)
+}
+
+pub fn part2(input: &str) -> usize {
+    polymerize(input, 40)
+}
