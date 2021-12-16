27e3bc0ec848d683a8087d2be077cee8070d1c30
Dan Seminara <semi225599@gmail.com>
2021-12-16 10:34:18 -0500

    Shorten

--- src/year2021/day16.rs
+++ src/year2021/day16.rs
@@ -1,43 +1,31 @@
-fn take<'a>(bits: &mut &'a [u64], n: usize) -> &'a [u64] {
-    let r = &bits[..n];
-    *bits = &bits[n..];
-    r
+fn take<'a>(n: usize, bits: &mut &'a [u64]) -> &'a [u64] {
+    &std::mem::replace(bits, &bits[n..])[..n]
 }
 
 fn bin(ds: &[u64]) -> u64 {
     ds.iter().fold(0, |a, b| a << 1 | b)
 }
 
-fn packet(bs: &mut &[u64]) -> (u64, u64) {
-    let mut version = bin(take(bs, 3));
-    let type_id = bin(take(bs, 3));
+fn packet(bs: &mut &[u64], vsum: &mut u64) -> u64 {
+    *vsum += bin(take(3, bs));
+    let type_id = bin(take(3, bs));
     if type_id == 4 {
         let mut n = 0;
-        loop {
-            let ds = take(bs, 5);
-            n = n << 4 | bin(&ds[1..]);
-            if ds[0] == 0 {
-                return (version, n);
-            }
+        while take(1, bs)[0] == 1 {
+            n = n << 4 | bin(take(4, bs));
         }
+        return n << 4 | bin(take(4, bs));
     }
     let mut ns = vec![];
-    if take(bs, 1)[0] == 0 {
-        let n = bin(take(bs, 15)) as usize;
-        let mut r = take(bs, n);
+    if take(1, bs)[0] == 0 {
+        let mut r = take(bin(take(15, bs)) as usize, bs);
         while !r.is_empty() {
-            let (v, a) = packet(&mut r);
-            version += v;
-            ns.push(a);
+            ns.push(packet(&mut r, vsum));
         }
     } else {
-        for _ in 0..bin(take(bs, 11)) {
-            let (v, a) = packet(bs);
-            version += v;
-            ns.push(a);
-        }
+        (0..bin(take(11, bs))).for_each(|_| ns.push(packet(bs, vsum)));
     }
-    let n = match type_id {
+    match type_id {
         0 => ns.into_iter().sum(),
         1 => ns.into_iter().product(),
         2 => ns.into_iter().min().unwrap(),
@@ -46,19 +34,17 @@ fn packet(bs: &mut &[u64]) -> (u64, u64) {
         6 => (ns[0] < ns[1]) as u64,
         7 => (ns[0] == ns[1]) as u64,
         _ => panic!("Bad type id: {}", type_id),
-    };
-    (version, n)
+    }
 }
 
 fn solve(input: &str) -> (u64, u64) {
     let bits = input
         .chars()
-        .flat_map(|c| {
-            let n = c.to_digit(16).unwrap() as u64;
-            vec![n >> 3 & 1, n >> 2 & 1, n >> 1 & 1, n & 1]
-        })
-        .collect::<Vec<_>>();
-    packet(&mut &bits[..])
+        .map(|c| c.to_digit(16).unwrap() as u64)
+        .flat_map(|n| vec![n >> 3 & 1, n >> 2 & 1, n >> 1 & 1, n & 1]);
+    let mut version = 0;
+    let n = packet(&mut &bits.collect::<Vec<_>>()[..], &mut version);
+    (version, n)
 }
 
 pub fn part1(input: &str) -> u64 {
