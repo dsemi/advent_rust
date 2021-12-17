155784abc5421307e93f3aaee6638d378b771102
Dan Seminara <semi225599@gmail.com>
2021-12-17 15:38:00 -0500

    Shorten and speed up checks

--- src/year2021/day17.rs
+++ src/year2021/day17.rs
@@ -1,23 +1,24 @@
 use scan_fmt::scan_fmt as scanf;
 use std::cmp::max;
 
-fn max_height(x0: i64, x1: i64, y0: i64, y1: i64, mut vx: i64, mut vy: i64) -> Option<i64> {
-    let (mut x, mut y): (i64, i64) = (0, 0);
-    let mut max_y = 0;
-    while y >= y0 {
-        if x0 <= x && x <= x1 && y0 <= y && y <= y1 {
-            return Some(max_y);
-        }
-        x += vx;
-        y += vy;
-        max_y = max(max_y, y);
-        vx = max(0, vx - 1);
-        vy -= 1;
-    }
-    None
+fn hits_target(x0: i64, x1: i64, y0: i64, y1: i64, mut vx: i64, mut vy: i64) -> bool {
+    (0..)
+        .scan((0, 0), |p, _| {
+            p.0 += vx;
+            p.1 += vy;
+            vx = max(0, vx - 1);
+            vy -= 1;
+            (p.0 <= x1 && p.1 >= y0).then(|| *p)
+        })
+        .any(|(x, y)| x0 <= x && x <= x1 && y0 <= y && y <= y1)
 }
 
-fn heights(input: &str) -> impl Iterator<Item = i64> {
+pub fn part1(input: &str) -> i64 {
+    let y0 = scanf!(input, "target area: x={*d}..{*d}, y={}..{*d}", i64).unwrap();
+    y0 * (y0 + 1) / 2
+}
+
+pub fn part2(input: &str) -> usize {
     let (x0, x1, y0, y1) =
         scanf!(input, "target area: x={}..{}, y={}..{}", i64, i64, i64, i64).unwrap();
     assert!(x0 > 0 && x1 > 0);
@@ -25,13 +26,7 @@ fn heights(input: &str) -> impl Iterator<Item = i64> {
     // First triangular number > x0 is lower bound.
     // n^2 + n - 2x0 = 0
     let mx = ((1.0 + 8.0 * x0 as f64).sqrt() / 2.0 - 0.5).ceil() as i64;
-    (mx..=x1).flat_map(move |x| (y0..=-y0).filter_map(move |y| max_height(x0, x1, y0, y1, x, y)))
-}
-
-pub fn part1(input: &str) -> Option<i64> {
-    heights(input).max()
-}
-
-pub fn part2(input: &str) -> usize {
-    heights(input).count()
+    (mx..=x1)
+        .flat_map(|x| (y0..=-y0).filter(move |y| hits_target(x0, x1, y0, y1, x, *y)))
+        .count()
 }
