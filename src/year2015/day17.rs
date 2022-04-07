628f369afcc597b9118442379a2684a43be46630
Dan Seminara <semi225599@gmail.com>
2022-04-07 15:33:20 -0400

    Speed up 2015 Day 17

--- src/year2015/day17.rs
+++ src/year2015/day17.rs
@@ -1,18 +1,44 @@
-use crate::utils::Combinations;
-use streaming_iterator::StreamingIterator;
-
-fn all_combos(xs: Vec<i32>) -> impl Iterator<Item = usize> {
-    (1..=xs.len()).map(move |n| {
-        Combinations::new(&xs, n)
-            .filter(|combo| combo.iter().copied().sum::<i32>() == 150)
-            .count()
-    })
+struct Combos<'a> {
+    stack: Vec<(&'a [i32], i32, usize)>,
 }
 
-pub fn part1(input: Vec<i32>) -> usize {
-    all_combos(input).sum()
+impl<'a> Combos<'a> {
+    fn new(input: &'a mut [i32]) -> Self {
+        input.sort_unstable_by_key(|k| -k);
+        return Self {
+            stack: vec![(input, 150, 0)],
+        };
+    }
 }
 
-pub fn part2(input: Vec<i32>) -> Option<usize> {
-    all_combos(input).find(|v| v > &0)
+impl Iterator for Combos<'_> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((containers, nog, len)) = self.stack.pop() {
+            if nog == 0 {
+                return Some(len);
+            }
+            if let Some((i, n)) = containers
+                .iter()
+                .enumerate()
+                .skip_while(|(_, &x)| x > nog)
+                .next()
+            {
+                self.stack.push((&containers[i + 1..], nog, len));
+                self.stack.push((&containers[i + 1..], nog - n, len + 1));
+            }
+        }
+        None
+    }
+}
+
+pub fn part1(mut input: Vec<i32>) -> usize {
+    Combos::new(&mut input).count()
+}
+
+pub fn part2(mut input: Vec<i32>) -> usize {
+    let lengths = Combos::new(&mut input).collect::<Vec<_>>();
+    let min = lengths.iter().min().unwrap();
+    lengths.iter().filter(|&x| x == min).count()
 }
