36c6bb21066048124237db5d991ecc4920ab0d9c
Dan Seminara <semi225599@gmail.com>
2022-05-30 10:32:24 -0400

    Remove set

--- src/year2021/day04.rs
+++ src/year2021/day04.rs
@@ -1,15 +1,11 @@
-use ahash::AHashSet;
-
-fn is_winner(brd: &[Vec<u32>], ns: &AHashSet<u32>) -> bool {
-    (0..brd.len()).any(|i| {
-        (0..brd.len()).all(|j| ns.contains(&brd[i][j]))
-            || (0..brd.len()).all(|j| ns.contains(&brd[j][i]))
-    })
+fn is_winner(brd: &[Vec<i32>]) -> bool {
+    (0..brd.len())
+        .any(|i| (0..brd.len()).all(|j| brd[i][j] == -1) || (0..brd.len()).all(|j| brd[j][i] == -1))
 }
 
-fn winner_scores(input: &str) -> impl Iterator<Item = u32> + '_ {
+fn winner_scores(input: &str) -> impl Iterator<Item = i32> + '_ {
     let (nums, board_str) = input.split_once("\n\n").unwrap();
-    let mut boards: Vec<Vec<Vec<u32>>> = board_str
+    let mut boards: Vec<Vec<Vec<i32>>> = board_str
         .split("\n\n")
         .map(|board| {
             board
@@ -18,22 +14,20 @@ fn winner_scores(input: &str) -> impl Iterator<Item = u32> + '_ {
                 .collect()
         })
         .collect();
-    let mut called = AHashSet::new();
     nums.split(',').flat_map(move |n| {
-        let n = n.parse().unwrap();
-        called.insert(n);
-        // drain_filter (can also mutate w/ drain_filter, removes need for HashSet,
-        // set called ns to 0).
+        let n = n.parse::<i32>().unwrap();
+        // drain_filter.
         let mut winners = vec![];
-        boards.retain(|b| {
-            if is_winner(b, &called) {
-                winners.push(
-                    b.iter()
-                        .flatten()
-                        .filter(|n| !called.contains(n))
-                        .sum::<u32>()
-                        * n,
-                );
+        boards.retain_mut(|b| {
+            for row in b.iter_mut() {
+                for v in row.iter_mut() {
+                    if *v == n {
+                        *v = -1;
+                    }
+                }
+            }
+            if is_winner(b) {
+                winners.push(b.iter().flatten().filter(|n| *n != &-1).sum::<i32>() * n);
                 return false;
             }
             true
@@ -42,10 +36,10 @@ fn winner_scores(input: &str) -> impl Iterator<Item = u32> + '_ {
     })
 }
 
-pub fn part1(input: &str) -> Option<u32> {
+pub fn part1(input: &str) -> Option<i32> {
     winner_scores(input).next()
 }
 
-pub fn part2(input: &str) -> Option<u32> {
+pub fn part2(input: &str) -> Option<i32> {
     winner_scores(input).last()
 }
