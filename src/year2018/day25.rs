5646ce191f0eeb1cbd6783e6062d70a2451a9989
Dan Seminara <semi225599@gmail.com>
2022-07-19 20:08:07 -0400

    Speed up 2018 Day 25 with union find

--- src/year2018/day25.rs
+++ src/year2018/day25.rs
@@ -1,3 +1,5 @@
+use ahash::AHashMap;
+
 type Coord4 = (i64, i64, i64, i64);
 
 fn parse_points(input: &str) -> Vec<Coord4> {
@@ -19,28 +21,51 @@ fn dist(a: Coord4, b: Coord4) -> i64 {
     (w0 - w1).abs() + (x0 - x1).abs() + (y0 - y1).abs() + (z0 - z1).abs()
 }
 
-fn constellations(mut pts: Vec<Coord4>) -> Vec<Vec<Coord4>> {
-    let mut result = Vec::new();
-    while let Some(neighb) = pts.pop() {
-        let mut neighbs = vec![neighb];
-        let mut changed = true;
-        while std::mem::replace(&mut changed, false) {
-            pts.retain(|p| {
-                if neighbs.iter().any(|x| dist(*x, *p) <= 3) {
-                    changed = true;
-                    neighbs.push(*p);
-                    return false;
-                }
-                true
-            });
-        }
-        result.push(neighbs);
+fn find(parent: &[usize], mut k: usize) -> usize {
+    while k != parent[k] {
+        k = parent[k]
     }
-    result
+    k
+}
+
+fn union(parent: &mut [usize], rank: &mut [usize], x: usize, y: usize) {
+    let x_root = find(parent, x);
+    let y_root = find(parent, y);
+    if x_root == y_root {
+        return;
+    }
+    if rank[x_root] < rank[y_root] {
+        parent[x_root] = y_root;
+    } else if rank[x_root] > rank[y_root] {
+        parent[y_root] = x_root;
+    } else {
+        parent[y_root] = x_root;
+        rank[x_root] += 1;
+    }
+}
+
+fn constellations(pts: Vec<Coord4>) -> usize {
+    let mut parent = vec![0; pts.len()];
+    for (i, p) in parent.iter_mut().enumerate() {
+        *p = i;
+    }
+    let mut rank = vec![0; pts.len()];
+    for i in 0..pts.len() {
+        for j in i + 1..pts.len() {
+            if dist(pts[i], pts[j]) <= 3 {
+                union(&mut parent, &mut rank, i, j);
+            }
+        }
+    }
+    let mut m = AHashMap::new();
+    for p in 0..pts.len() {
+        m.insert(find(&parent, p), true);
+    }
+    m.len()
 }
 
 pub fn part1(input: &str) -> usize {
-    constellations(parse_points(input)).len()
+    constellations(parse_points(input))
 }
 
 pub fn part2(_input: &str) -> String {
