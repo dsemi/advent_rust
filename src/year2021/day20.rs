d546bfc7f9b6ea0ef03a8c1c693394f199bac6f4
Dan Seminara <semi225599@gmail.com>
2022-10-06 19:29:43 -0400

    Bitwise stepping

--- src/year2021/day20.rs
+++ src/year2021/day20.rs
@@ -1,51 +1,62 @@
-fn pad<T: Clone + Copy>(grid: &mut Vec<Vec<T>>, c: T) {
-    grid.insert(0, vec![c; grid[0].len()]);
-    grid.push(vec![c; grid[0].len()]);
-    for row in grid.iter_mut() {
-        row.insert(0, c);
-        row.push(c);
-    }
-}
-
-fn enhance(iea: &[u8], grid: &mut Vec<Vec<u8>>) {
-    pad(grid, grid[0][0]);
-    let mut grid2 = grid.clone();
-    for r in 1..grid.len() - 1 {
-        for c in 1..grid[r].len() - 1 {
-            let idx = adj_w_diag!(grid, r, c)
-                .into_iter()
-                .fold(0, |a, b| a << 1 | (b == b'#') as usize);
-            grid2[r][c] = iea[idx];
-        }
-    }
-    std::mem::swap(grid, &mut grid2);
-    let ch = iea[vec![grid[0][0]; 9]
-        .into_iter()
-        .fold(0, |a, b| a << 1 | (b == b'#') as usize)];
-    let last = grid.len() - 1;
-    for (i, row) in grid.iter_mut().enumerate() {
-        row[0] = ch;
-        *row.last_mut().unwrap() = ch;
-        if i == 0 || i == last {
-            row.iter_mut().for_each(|v| *v = ch);
+fn advance(grid: &mut [u8], quads: &[u8], xor_in: u16, xor_out: u8, dim: usize) {
+    let mut prev = [0_u8; 100];
+    for r in 0..dim {
+        let mut quad = 0_u16;
+        for c in 0..dim {
+            let p = 100 * r + c;
+            quad = (quad & 0x3333) << 2;
+            quad |= ((prev[c] as u16) << 8) | grid[p] as u16;
+            prev[c] = grid[p];
+            grid[p] = quads[(quad ^ xor_in) as usize] ^ xor_out;
         }
     }
 }
 
-fn run(input: &str, times: usize) -> usize {
+fn run(input: &str, times: usize) -> u32 {
     let (iea, img) = input.split_once("\n\n").unwrap();
-    let mut im: Vec<Vec<u8>> = img.lines().map(|line| line.bytes().collect()).collect();
-    pad(&mut im, b'.');
-    (0..times).for_each(|_| enhance(iea.as_bytes(), &mut im));
-    im.into_iter()
-        .map(|row| row.into_iter().filter(|&v| v == b'#').count())
-        .sum()
+    let mut rules = [0_u8; 2048];
+    let mut idx = 0;
+    for c in iea.bytes() {
+        rules[idx] = c & 1;
+        idx = (idx + 0x88 + 1) & 0x777;
+    }
+    let mut quads = [0_u8; 65536];
+    for i in 0..quads.len() {
+        let mut quad = 0;
+        quad |= rules[(i >> 5) & 0x777] << 5;
+        quad |= rules[(i >> 4) & 0x777] << 4;
+        quad |= rules[(i >> 1) & 0x777] << 1;
+        quad |= rules[(i >> 0) & 0x777] << 0;
+        quads[i] = quad;
+    }
+    let inp = img.as_bytes();
+    let mut grid = [0_u8; 10000];
+    idx = 0;
+    for r in 0..50 {
+        for c in 0..50 {
+            let mut quad = 0;
+            quad |= (inp[idx + 0] & 1) << 5;
+            quad |= (inp[idx + 1] & 1) << 4;
+            quad |= (inp[idx + 101] & 1) << 1;
+            quad |= (inp[idx + 102] & 1) << 0;
+            grid[100 * r + c] = quad;
+            idx += 2;
+        }
+        idx += 102;
+    }
+    let xor_in = if quads[0] != 0 { 0xffff } else { 0 };
+    let xor_out = if quads[0] != 0 { 0x33 } else { 0 };
+    for step in (0..times).step_by(2) {
+        advance(&mut grid, &quads, 0, xor_out, 50 + (step + 1));
+        advance(&mut grid, &quads, xor_in, 0, 50 + (step + 2));
+    }
+    grid.into_iter().map(|x| x.count_ones()).sum()
 }
 
-pub fn part1(input: &str) -> usize {
+pub fn part1(input: &str) -> u32 {
     run(input, 2)
 }
 
-pub fn part2(input: &str) -> usize {
+pub fn part2(input: &str) -> u32 {
     run(input, 50)
 }
