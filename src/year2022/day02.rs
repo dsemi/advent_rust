687f1efcce5b5e1497a3ba760faac10af218bae9
Dan Seminara <semi225599@gmail.com>
2022-12-02 11:55:56 -0500

    Shorten

--- src/year2022/day02.rs
+++ src/year2022/day02.rs
@@ -1,26 +1,17 @@
-fn solve(input: &str, conv: fn(i32, i32) -> i32) -> i32 {
+fn solve(input: &str, f: fn(i32, i32) -> i32) -> i32 {
     input
         .lines()
         .map(|line| {
             let pts = line.as_bytes();
-            let a = (pts[0] - b'A') as i32;
-            let b = conv((pts[2] - b'X') as i32, a);
-            let wld = if b == (a + 1) % 3 {
-                6
-            } else if b == a {
-                3
-            } else {
-                0
-            };
-            wld + b + 1
+            f((pts[0] - b'A') as i32, (pts[2] - b'X') as i32)
         })
         .sum()
 }
 
 pub fn part1(input: &str) -> i32 {
-    solve(input, |c, _| c)
+    solve(input, |a, b| 3 * (b - a + 1).rem_euclid(3) + b + 1)
 }
 
 pub fn part2(input: &str) -> i32 {
-    solve(input, |c, a| (a + c - 1).rem_euclid(3))
+    solve(input, |a, b| (a + b - 1).rem_euclid(3) + 3 * b + 1)
 }
