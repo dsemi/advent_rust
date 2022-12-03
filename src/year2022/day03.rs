47c06028c5ebe7ad969185eb288c4829777a781b
Dan Seminara <semi225599@gmail.com>
2022-12-03 00:45:57 -0500

    2022 Day 3

---
+++ src/year2022/day03.rs
@@ -0,0 +1,35 @@
+use ahash::AHashSet;
+
+const S: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+pub fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let a = line[..line.len() / 2].chars().collect::<AHashSet<_>>();
+            let b = line[line.len() / 2..].chars().collect::<AHashSet<_>>();
+            let ch = *a.intersection(&b).next().unwrap();
+            S.chars().position(|c| c == ch).unwrap()
+        })
+        .sum()
+}
+
+pub fn part2(input: &str) -> usize {
+    let lines = input.lines().collect::<Vec<_>>();
+    lines
+        .chunks(3)
+        .map(|pts| {
+            let a = pts[0].chars().collect::<AHashSet<_>>();
+            let b = pts[1].chars().collect::<AHashSet<_>>();
+            let c = pts[2].chars().collect::<AHashSet<_>>();
+            let ch = *a
+                .intersection(&b)
+                .copied()
+                .collect::<AHashSet<_>>()
+                .intersection(&c)
+                .next()
+                .unwrap();
+            S.chars().position(|c| c == ch).unwrap()
+        })
+        .sum()
+}
