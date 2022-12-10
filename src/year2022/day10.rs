b932d3e96e449cd1d19430cd9fbbba990f39d7c1
Dan Seminara <semi225599@gmail.com>
2022-12-10 11:57:05 -0500

    Shorten

--- src/year2022/day10.rs
+++ src/year2022/day10.rs
@@ -1,6 +1,6 @@
 use crate::utils::*;
 
-fn run(input: &str) -> impl Iterator<Item = (usize, i32)> + '_ {
+fn run(input: &str) -> impl Iterator<Item = i32> + '_ {
     input
         .split_whitespace()
         .map(|tok| match tok {
@@ -8,32 +8,24 @@ fn run(input: &str) -> impl Iterator<Item = (usize, i32)> + '_ {
             _ => tok.parse().unwrap(),
         })
         .good_scan(1, |x, d| x + d)
-        .enumerate()
-        .map(|(i, x)| (i + 1, x))
         .take(240)
 }
 
 pub fn part1(input: &str) -> i32 {
     run(input)
-        .filter_map(|(cycle, x)| {
-            [20, 60, 100, 140, 180, 220]
-                .contains(&cycle)
-                .then(|| cycle as i32 * x)
-        })
+        .enumerate()
+        .filter_map(|(c, x)| ((c + 1) % 40 == 20).then(|| (c + 1) as i32 * x))
         .sum()
 }
 
 pub fn part2(input: &str) -> String {
-    let mut res = "\n".to_owned();
-    for (cycle, x) in run(input) {
-        res.push(if ((cycle as i32 - 1) % 40 - x).abs() <= 1 {
-            '#'
-        } else {
-            ' '
-        });
-        if cycle < 240 && cycle % 40 == 0 {
+    let mut res = String::new();
+    for (c, x) in run(input).enumerate() {
+        let m = c as i32 % 40;
+        if m == 0 {
             res.push('\n');
         }
+        res.push(if (m - x).abs() <= 1 { '#' } else { ' ' });
     }
     res
 }
