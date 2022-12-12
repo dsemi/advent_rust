34af8d0e61700427eb373f654750f54b2e124340
Dan Seminara <semi225599@gmail.com>
2022-12-12 01:52:56 -0500

    Small tidy

--- src/year2022/day12.rs
+++ src/year2022/day12.rs
@@ -21,28 +21,20 @@ fn neighbors(grid: &Vec<Vec<u8>>, pos: &Coord<i32>) -> Vec<Coord<i32>> {
     .collect()
 }
 
-fn solve(input: &str, p2: bool) -> usize {
+fn solve(input: &str, sts: &[u8]) -> usize {
     let mut starts = Vec::new();
     let mut done = Coord::new(0, 0);
-    let mut grid = Vec::new();
-    for (r, line) in input.lines().enumerate() {
-        grid.push(Vec::new());
-        for (c, v) in line.chars().enumerate() {
-            match v {
-                'S' => {
-                    starts.push(Coord::new(r as i32, c as i32));
-                    grid.last_mut().unwrap().push(0);
-                }
-                'E' => {
-                    done = Coord::new(r as i32, c as i32);
-                    grid.last_mut().unwrap().push(25);
-                }
-                _ => {
-                    if p2 && v == 'a' {
-                        starts.push(Coord::new(r as i32, c as i32));
-                    }
-                    grid.last_mut().unwrap().push(v as u8 - b'a');
-                }
+    let mut grid: Vec<Vec<u8>> = input.lines().map(|l| l.bytes().collect()).collect();
+    for (r, row) in grid.iter_mut().enumerate() {
+        for (c, v) in row.iter_mut().enumerate() {
+            if sts.contains(v) {
+                starts.push(Coord::new(r as i32, c as i32));
+            }
+            if v == &b'S' {
+                *v = b'a';
+            } else if v == &b'E' {
+                done = Coord::new(r as i32, c as i32);
+                *v = b'z';
             }
         }
     }
@@ -55,9 +47,9 @@ fn solve(input: &str, p2: bool) -> usize {
 }
 
 pub fn part1(input: &str) -> usize {
-    solve(input, false)
+    solve(input, &[b'S'])
 }
 
 pub fn part2(input: &str) -> usize {
-    solve(input, true)
+    solve(input, &[b'S', b'a'])
 }
