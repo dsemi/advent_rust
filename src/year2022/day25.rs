aa857b4d84f770613a30283bed5e5741ba50c87e
Dan Seminara <semi225599@gmail.com>
2023-01-28 00:06:24 -0500

    Shorten

--- src/year2022/day25.rs
+++ src/year2022/day25.rs
@@ -1,19 +1,15 @@
-const IN: &str = "=-012";
-const OUT: [char; 5] = ['0', '1', '2', '=', '-'];
-
 pub fn part1(input: &str) -> String {
-    let mut num = 0;
-    for line in input.lines() {
-        for (i, c) in line.chars().rev().enumerate() {
-            num += 5i64.pow(i as u32) * (IN.chars().position(|i| i == c).unwrap() as i64 - 2);
-        }
-    }
-    let mut res = Vec::new();
-    while num > 0 {
-        res.push(OUT[(num % 5) as usize]);
-        num = (num + 2) / 5;
-    }
-    res.into_iter().rev().collect()
+    const IN: &str = "=-012";
+    const OUT: [char; 5] = ['0', '1', '2', '=', '-'];
+    let d: ahash::AHashMap<char, i64> =
+        IN.chars().enumerate().map(|(i, c)| (c, i as i64)).collect();
+    let n: i64 = input
+        .lines()
+        .map(|l| l.chars().map(|c| d[&c] - 2).fold(0, |a, b| a * 5 + b))
+        .sum();
+    let ds: Vec<i64> =
+        itertools::unfold(n, |n| (*n > 0).then(|| std::mem::replace(n, (*n + 2) / 5))).collect();
+    ds.iter().rev().map(|n| OUT[(n % 5) as usize]).collect()
 }
 
 pub fn part2(_input: &str) -> String {
