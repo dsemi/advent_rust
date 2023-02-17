37521d6ae80f06acb9e42d3cee6cad69d0172b1a
Dan Seminara <semi225599@gmail.com>
2023-02-17 17:37:55 -0500

    Shorten

--- src/year2022/day21.rs
+++ src/year2022/day21.rs
@@ -2,7 +2,6 @@ use ahash::AHashMap;
 use num_complex::Complex;
 use num_rational::Ratio;
 use std::ops::{Add, Div, Mul, Sub};
-use Monkey::*;
 
 type N = Complex<Ratio<i64>>;
 
@@ -13,8 +12,8 @@ enum Monkey<'a> {
 
 fn eval(m: &AHashMap<&str, Monkey>, k: &str) -> N {
     match &m[k] {
-        Num(n) => *n,
-        Math(op, l, r) => op(eval(m, l), eval(m, r)),
+        Monkey::Num(n) => *n,
+        Monkey::Math(op, l, r) => op(eval(m, l), eval(m, r)),
     }
 }
 
@@ -24,7 +23,7 @@ fn monkeys(input: &str) -> AHashMap<&str, Monkey> {
         .map(|line| {
             let pts = line.split_whitespace().collect::<Vec<_>>();
             let monkey = match pts[1..] {
-                [n] => Num(n.parse().unwrap()),
+                [n] => Monkey::Num(n.parse().unwrap()),
                 [a, op, b] => {
                     let f = match op.chars().next().unwrap() {
                         '+' => Add::add,
@@ -33,7 +32,7 @@ fn monkeys(input: &str) -> AHashMap<&str, Monkey> {
                         '/' => Div::div,
                         _ => panic!("Bad op: {}", op),
                     };
-                    Math(f, a, b)
+                    Monkey::Math(f, a, b)
                 }
                 _ => panic!("Bad parse: {}", line),
             };
@@ -43,14 +42,13 @@ fn monkeys(input: &str) -> AHashMap<&str, Monkey> {
 }
 
 pub fn part1(input: &str) -> i64 {
-    let m = monkeys(input);
-    *eval(&m, "root").re.numer()
+    *eval(&monkeys(input), "root").re.numer()
 }
 
 pub fn part2(input: &str) -> i64 {
     let mut m = monkeys(input);
-    m.insert("humn", Num(Complex::i()));
-    if let Some(Math(f, _, _)) = m.get_mut("root") {
+    m.insert("humn", Monkey::Num(Complex::i()));
+    if let Some(Monkey::Math(f, _, _)) = m.get_mut("root") {
         *f = Sub::sub;
     }
     let n = eval(&m, "root");
