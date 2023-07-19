77260661571baeffb8afd8ae2bf3181e60755dbb
Dan Seminara <semi225599@gmail.com>
2023-07-19 00:49:03 -0400

    Small improvements

--- src/year2019/day01.rs
+++ src/year2019/day01.rs
@@ -6,6 +6,6 @@ pub fn part1(ns: Vec<i64>) -> i64 {
 
 pub fn part2(ns: Vec<i64>) -> i64 {
     ns.into_iter()
-        .flat_map(|n| iterate(n / 3 - 2, |fuel| fuel / 3 - 2).take_while(|fuel| fuel > &0))
+        .flat_map(|n| iterate(n / 3 - 2, |fuel| fuel / 3 - 2).take_while(|&fuel| fuel > 0))
         .sum()
 }
--- src/year2019/day02.rs
+++ src/year2019/day02.rs
@@ -1,17 +1,15 @@
 use super::intcode;
+use rayon::prelude::*;
 
 pub fn part1(input: &str) -> i64 {
     intcode::new(input).run_no_io(12, 2)
 }
 
-pub fn part2(input: &str) -> i64 {
+pub fn part2(input: &str) -> Option<i64> {
     let prog = intcode::new(input);
-    for noun in 0..99 {
-        for verb in 0..99 {
-            if prog.clone().run_no_io(noun, verb) == 19690720 {
-                return 100 * noun + verb;
-            }
-        }
-    }
-    0
+    (0..100).find_map(|noun| {
+        (0..100).into_par_iter().find_map_any(|verb| {
+            (prog.clone().run_no_io(noun, verb) == 19690720).then(|| 100 * noun + verb)
+        })
+    })
 }
