4f19cd6638912c1011010e0205fed68a4503443e
Dan Seminara <semi225599@gmail.com>
2023-11-30 20:29:33 -0500

    Improve performance

--- src/year2022/day06.rs
+++ src/year2022/day06.rs
@@ -1,13 +1,30 @@
-use itertools::Itertools;
-
-fn solve(input: &str, nchars: usize) -> Option<usize> {
-    (nchars..=input.len()).find(|&i| input[i - nchars..i].chars().all_unique())
+fn solve(input: &[u8], nchars: usize) -> Option<usize> {
+    let mut i = nchars as usize;
+    while i < input.len() {
+        let unique = input[i - nchars..i]
+            .iter()
+            .rev()
+            .enumerate()
+            .try_fold(0u32, |set, (j, b)| {
+                let bit = 1 << (b - b'a');
+                if set & bit == 0 {
+                    Ok(set | bit)
+                } else {
+                    Err(nchars - j)
+                }
+            });
+        match unique {
+            Ok(_) => return Some(i),
+            Err(n) => i += n,
+        }
+    }
+    None
 }
 
-pub fn part1(input: &str) -> Option<usize> {
+pub fn part1(input: &[u8]) -> Option<usize> {
     solve(input, 4)
 }
 
-pub fn part2(input: &str) -> Option<usize> {
+pub fn part2(input: &[u8]) -> Option<usize> {
     solve(input, 14)
 }
