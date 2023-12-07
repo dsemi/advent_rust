8f98ba326f2bc773b6d120a559227b77e1f48d8e
Dan Seminara <semi225599@gmail.com>
2023-12-07 04:54:41 -0500

    Simplify

--- src/year2023/day07.rs
+++ src/year2023/day07.rs
@@ -12,27 +12,50 @@ enum Hand {
     FiveOfAKind,
 }
 
-fn freq(i: &[u8]) -> u64 {
-    i.iter().fold(0, |acc, b| acc + (1 << (3 * b)))
-}
+const FOUR: u64 = 0x124924924924;
+const TWO: u64 = FOUR >> 1;
 
-fn hand(bits: u64) -> Hand {
-    let ones = bits.count_ones();
-    match (ones, bits & (bits << 2) > 0, bits & (bits << 1) > 0) {
-        (2, true, _) => FiveOfAKind,
-        (2, false, _) => FourOfAKind,
-        (3, _, true) => FullHouse,
-        (3, _, false) => TwoPair,
-        (4, _, true) => ThreeOfAKind,
-        (4, _, false) => OnePair,
-        (_, _, _) => HighCard,
+fn hand(cards: &[u8]) -> Hand {
+    let mut bits = cards.iter().fold(0, |acc, b| acc + (1 << (3 * b)));
+    let jokers = bits & 0b111;
+    bits >>= 3;
+    if jokers >= 4 {
+        return FiveOfAKind;
+    }
+    if bits & FOUR > 0 {
+        bits += jokers << ((bits & FOUR).trailing_zeros() - 2);
+    } else if bits & (bits << 1) & TWO > 0 {
+        bits += jokers << ((bits & (bits << 1) & TWO).trailing_zeros() - 1);
+    } else if bits & TWO > 0 {
+        bits += jokers << ((bits & TWO).trailing_zeros() - 1);
+    } else {
+        bits += jokers << bits.trailing_zeros();
+    }
+    if bits & (bits << 2) & FOUR > 0 {
+        FiveOfAKind
+    } else if bits & FOUR > 0 {
+        FourOfAKind
+    } else if bits & (bits << 1) & TWO > 0 {
+        if (bits & TWO).count_ones() == 2 {
+            FullHouse
+        } else {
+            ThreeOfAKind
+        }
+    } else if bits & TWO > 0 {
+        if (bits & TWO).count_ones() == 2 {
+            TwoPair
+        } else {
+            OnePair
+        }
+    } else {
+        HighCard
     }
 }
 
-fn val(a: u8) -> u8 {
+fn val(j: u8, a: u8) -> u8 {
     match a {
         b'T' => 10,
-        b'J' => 11,
+        b'J' => j,
         b'Q' => 12,
         b'K' => 13,
         b'A' => 14,
@@ -44,10 +67,10 @@ fn parse(i: &str) -> [u8; 5] {
     i.as_bytes().try_into().unwrap()
 }
 
-fn solve(input: &str, hand: fn(&[u8]) -> Hand, val: fn(u8) -> u8) -> usize {
+fn solve(input: &str, j: u8) -> usize {
     let mut hands: Vec<_> = lines_iter(input, separated_pair(alphanumeric1, space1, usize))
         .map(|(cards, bid)| {
-            let cards = parse(cards).map(val);
+            let cards = parse(cards).map(|c| val(j, c));
             (hand(&cards), cards, bid)
         })
         .collect();
@@ -60,37 +83,9 @@ fn solve(input: &str, hand: fn(&[u8]) -> Hand, val: fn(u8) -> u8) -> usize {
 }
 
 pub fn part1(input: &str) -> usize {
-    solve(input, |s| hand(freq(s)), val)
-}
-
-fn hand2(i: &[u8]) -> Hand {
-    let mut bits = freq(i);
-    let jokers = bits & 0b111;
-    bits >>= 3;
-    let ones = bits.count_ones();
-    match (jokers, ones, bits & (bits << 1) > 0) {
-        (0, _, _) => hand(bits),
-        (1, 1, _) => FiveOfAKind,
-        (1, 2, _) => FullHouse,
-        (1, 3, true) => FourOfAKind,
-        (1, 3, false) => ThreeOfAKind,
-        (1, 4, _) => OnePair,
-        (2, 2, true) => FiveOfAKind,
-        (2, 2, false) => FourOfAKind,
-        (2, 3, _) => ThreeOfAKind,
-        (3, 1, _) => FiveOfAKind,
-        (3, 2, _) => FourOfAKind,
-        (_, _, _) => FiveOfAKind,
-    }
-}
-
-fn val2(a: u8) -> u8 {
-    match a {
-        b'J' => 0,
-        b => val(b),
-    }
+    solve(input, 11)
 }
 
 pub fn part2(input: &str) -> usize {
-    solve(input, hand2, val2)
+    solve(input, 0)
 }
