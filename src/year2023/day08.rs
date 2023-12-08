831f9bb4fe5397b9b96e7d0785f5ea750af2c1e2
Dan Seminara <semi225599@gmail.com>
2023-12-08 01:48:28 -0500

    2023 Day 8

---
+++ src/year2023/day08.rs
@@ -0,0 +1,67 @@
+use crate::utils::parsers::*;
+use crate::utils::UniqueIdx;
+use ahash::AHashSet;
+use num::integer::lcm;
+use std::cmp::max;
+
+fn network(
+    input: &str,
+    is_start: fn(&str) -> bool,
+    is_end: fn(&str) -> bool,
+) -> (Vec<usize>, AHashSet<usize>, Vec<usize>, Vec<usize>) {
+    let mut starts = Vec::new();
+    let mut ends = Vec::new();
+    let mut lefts = Vec::new();
+    let mut rights = Vec::new();
+    let mut ui = UniqueIdx::new();
+    for line in input.lines() {
+        let (src, (left, right)) = separated_pair(
+            alphanumeric1,
+            " = ",
+            delimited('(', coord(alphanumeric1), ')'),
+        )
+        .read(line);
+        let k = ui.idx(src);
+        if is_start(src) {
+            starts.push(k);
+        }
+        if is_end(src) {
+            ends.push(k);
+        }
+        lefts.resize(max(lefts.len(), k + 1), 0);
+        lefts[k] = ui.idx(left);
+        rights.resize(max(lefts.len(), k + 1), 0);
+        rights[k] = ui.idx(right);
+    }
+    (starts, ends.into_iter().collect(), lefts, rights)
+}
+
+pub fn solve(input: &str, is_start: fn(&str) -> bool, is_end: fn(&str) -> bool) -> usize {
+    let (dirs, net) = separated_pair(alpha1, "\n\n", rest).read(input);
+    let (starts, ends, lefts, rights) = network(net, is_start, is_end);
+    starts
+        .into_iter()
+        .map(|node| {
+            dirs.chars()
+                .cycle()
+                .enumerate()
+                .try_fold(node, |node, (i, d)| {
+                    if ends.contains(&node) {
+                        Err(i)
+                    } else {
+                        Ok(if d == 'L' { lefts[node] } else { rights[node] })
+                    }
+                })
+                .unwrap_err()
+        })
+        .reduce(lcm)
+        .unwrap()
+}
+
+pub fn part1(input: &str) -> usize {
+    solve(input, |s| s == "AAA", |e| e == "ZZZ")
+}
+
+pub fn part2(input: &str) -> usize {
+    solve(input, |s| s.ends_with("A"), |e| e.ends_with("Z"))
+}
