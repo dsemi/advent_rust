b310862d2bf7b49a922aa18611c93217d3acd1b9
Dan Seminara <semi225599@gmail.com>
2023-12-15 14:06:54 -0500

    Bit shifts to speed up impl

--- src/year2023/day14.rs
+++ src/year2023/day14.rs
@@ -1,62 +1,112 @@
 use ahash::AHashMap;
-use ndarray::{Array, Axis, Dim};
 use std::collections::hash_map::Entry::*;
+use std::ops::{Shl, Shr};
 
-type Grid = Array<u8, Dim<[usize; 2]>>;
+enum Dir {
+    North,
+    West,
+    South,
+    East,
+}
 
-fn tilt(grid: &mut Grid, axis: Axis, d: i32) {
-    grid.axis_iter_mut(axis).for_each(|mut sl| {
-        let len = sl.len();
-        let mut to = if d > 0 { len - 1 } else { 0 };
-        for i in (0..len).map(|i| if d > 0 { len - 1 - i } else { i }) {
-            if sl[i] == b'O' {
-                sl[i] = b'.';
-                sl[to] = b'O';
-                to -= d as usize;
-            } else if sl[i] == b'#' {
-                to = i - d as usize;
+#[derive(Clone, Eq, PartialEq, Hash)]
+struct Grid {
+    rows: usize,
+    cols: usize,
+    cube: Vec<u128>,
+    round: Vec<u128>,
+}
+
+impl Grid {
+    fn parse(input: &str) -> Self {
+        let mut cube = Vec::new();
+        let mut round = Vec::new();
+        let mut cols = 0;
+        for line in input.lines() {
+            cols = line.len();
+            let mut cs = 0;
+            let mut rs = 0;
+            for b in line.bytes() {
+                cs <<= 1;
+                rs <<= 1;
+                match b {
+                    b'#' => cs |= 1,
+                    b'O' => rs |= 1,
+                    _ => (),
+                }
+            }
+            cube.push(cs);
+            round.push(rs);
+        }
+        assert!(cols <= 128);
+        Self {
+            rows: cube.len(),
+            cols,
+            cube,
+            round,
+        }
+    }
+
+    fn tilt_ns(&mut self, d: i32) {
+        let d = d as usize;
+        let mut moved = true;
+        while std::mem::take(&mut moved) {
+            for r in 1..self.rows {
+                let r = if d == 1 { self.rows - 1 - r } else { r };
+                let can_move = self.round[r] & !self.round[r + d] & !self.cube[r + d];
+                self.round[r] &= !can_move;
+                self.round[r + d] |= can_move;
+                moved |= can_move != 0;
             }
         }
-    });
-}
-
-fn load(grid: &Grid) -> usize {
-    let rows = grid.len_of(Axis(0));
-    grid.axis_iter(Axis(0))
-        .enumerate()
-        .map(|(i, row)| (rows - i) * row.iter().filter(|&v| *v == b'O').count())
-        .sum()
-}
-
-fn parse(input: &str) -> Grid {
-    let mut res = Vec::new();
-    let mut rows = 0;
-    let mut cols = 0;
-    for line in input.lines() {
-        cols = line.len();
-        rows += 1;
-        res.extend(line.bytes());
     }
-    Array::from_shape_vec((rows, cols), res).unwrap()
+
+    fn tilt_ew(&mut self, col: u128, f: fn(u128, u8) -> u128, s: fn(u128, u8) -> u128) {
+        let mut moved = true;
+        while std::mem::take(&mut moved) {
+            for (r, c) in self.round.iter_mut().zip(self.cube.iter()) {
+                let can_move = *r & !(f(*r | c, 1)) & !col;
+                *r = *r & !can_move | s(can_move, 1);
+                moved |= can_move != 0;
+            }
+        }
+    }
+
+    fn tilt(&mut self, dir: Dir) {
+        match dir {
+            Dir::North => self.tilt_ns(-1),
+            Dir::West => self.tilt_ew(1 << (self.cols - 1), u128::shr, u128::shl),
+            Dir::South => self.tilt_ns(1),
+            Dir::East => self.tilt_ew(1, u128::shl, u128::shr),
+        }
+    }
+
+    fn load(&self) -> usize {
+        self.round
+            .iter()
+            .enumerate()
+            .map(|(r, row)| (self.rows - r) * row.count_ones() as usize)
+            .sum()
+    }
 }
 
 pub fn part1(input: &str) -> usize {
-    let mut grid = parse(input);
-    tilt(&mut grid, Axis(1), -1);
-    load(&grid)
+    let mut grid = Grid::parse(input);
+    grid.tilt(Dir::North);
+    grid.load()
 }
 
-const CYCLES: usize = 1000000000;
+const CYCLES: usize = 1_000_000_000;
 
 fn cycle(grid: &mut Grid) {
-    tilt(grid, Axis(1), -1);
-    tilt(grid, Axis(0), -1);
-    tilt(grid, Axis(1), 1);
-    tilt(grid, Axis(0), 1);
+    grid.tilt(Dir::North);
+    grid.tilt(Dir::West);
+    grid.tilt(Dir::South);
+    grid.tilt(Dir::East);
 }
 
 pub fn part2(input: &str) -> usize {
-    let mut grid = parse(input);
+    let mut grid = Grid::parse(input);
     let mut visited = AHashMap::new();
     for i in 1..=CYCLES {
         cycle(&mut grid);
@@ -74,5 +124,5 @@ pub fn part2(input: &str) -> usize {
             }
         }
     }
-    load(&grid)
+    grid.load()
 }
