ed24fc074da83aa368c2421c97d295c7d03134c3
Dan Seminara <semi225599@gmail.com>
2023-12-15 00:43:53 -0500

    2023 Day 15

---
+++ src/year2023/day15.rs
@@ -0,0 +1,36 @@
+fn hash(input: &str) -> usize {
+    input.bytes().fold(0, |v, b| 17 * (v + b as usize) % 256)
+}
+
+pub fn part1(input: &str) -> usize {
+    input.split(',').map(hash).sum()
+}
+
+pub fn part2(input: &str) -> usize {
+    let mut boxes: Vec<Vec<(&str, usize)>> = vec![vec![]; 256];
+    for instr in input.split(',') {
+        if let Some((label, n)) = instr.split_once('=') {
+            let h = hash(label);
+            if let Some(e) = boxes[h].iter_mut().find(|(k, _)| *k == label) {
+                e.1 = n.parse().unwrap();
+            } else {
+                boxes[h].push((label, n.parse().unwrap()));
+            }
+        } else {
+            let label = &instr[0..instr.len() - 1];
+            let h = hash(label);
+            if let Some(i) = boxes[h].iter().position(|(k, _)| *k == label) {
+                boxes[h].remove(i);
+            }
+        }
+    }
+    boxes
+        .into_iter()
+        .enumerate()
+        .flat_map(|(n, b)| {
+            b.into_iter()
+                .enumerate()
+                .map(move |(s, (_, len))| (n + 1) * (s + 1) * len)
+        })
+        .sum()
+}
