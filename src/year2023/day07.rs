bf54efe253d505b707ef15086522b9959ea30a37
Dan Seminara <semi225599@gmail.com>
2024-01-09 16:52:30 -0500

    Speed up 2023 Day 7

--- src/year2023/day07.rs
+++ src/year2023/day07.rs
@@ -1,38 +1,70 @@
 use crate::utils::parsers::*;
-use std::cmp::Reverse;
+use Hand::*;
 
-fn val(j: usize, a: u8) -> usize {
+enum Hand {
+    HighCard,
+    Pair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+impl Hand {
+    fn add_card(&mut self, count: u8) {
+        match (&self, count) {
+            (HighCard, 2) => *self = Pair,
+            (Pair, 2) => *self = TwoPair,
+            (Pair, 3) => *self = ThreeOfAKind,
+            (ThreeOfAKind, 2) => *self = FullHouse,
+            (ThreeOfAKind, 4) => *self = FourOfAKind,
+            (FourOfAKind, 5) => *self = FiveOfAKind,
+            _ => (),
+        }
+    }
+}
+
+fn val(j: u8, a: u8) -> u8 {
     match a {
         b'T' => 10,
         b'J' => j,
         b'Q' => 12,
         b'K' => 13,
         b'A' => 14,
-        d => (d - b'0') as usize,
+        d => d - b'0',
     }
 }
 
-fn parse(i: &str) -> [u8; 5] {
-    i.as_bytes().try_into().unwrap()
-}
-
-fn solve(input: &str, j: usize) -> usize {
-    let mut hands: Vec<_> = lines_iter(input, separated_pair(alphanumeric1, space1, usize))
+fn solve(input: &str, j: u8) -> usize {
+    let mut hands: Vec<_> = lines_iter(input, separated_pair(alphanumeric1, space1, u64))
         .map(|(cards, bid)| {
-            let cards = parse(cards).map(|c| val(j, c));
-            let mut cnt = [0; 15];
-            cards.iter().for_each(|&c| cnt[c] += 1);
+            let mut cnt = [0_u8; 15];
+            let mut ord = [0_u8; 8];
+            let mut hand = HighCard;
+            cards.bytes().enumerate().for_each(|(i, c)| {
+                let v = val(j, c);
+                ord[i + 1] = v;
+                cnt[v as usize] += 1;
+                if v > 1 {
+                    hand.add_card(cnt[v as usize]);
+                }
+            });
             let jokers = std::mem::take(&mut cnt[1]);
-            cnt.sort_unstable_by_key(|&v| Reverse(v));
-            cnt[0] += jokers;
-            (cnt, cards, bid)
+            let most_freq_card = cnt.iter().enumerate().max_by_key(|x| x.1).unwrap().0;
+            for _ in 0..jokers {
+                cnt[most_freq_card] += 1;
+                hand.add_card(cnt[most_freq_card]);
+            }
+            ord[0] = hand as u8;
+            u64::from_be_bytes(ord) + bid
         })
         .collect();
     hands.sort_unstable();
     hands
         .into_iter()
         .enumerate()
-        .map(|(rank, (_, _, bid))| (rank + 1) * bid)
+        .map(|(rank, hand)| (rank + 1) * (hand & u16::MAX as u64) as usize)
         .sum()
 }
 
