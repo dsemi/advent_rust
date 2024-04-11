5b70bac08172eaa2ca339f8dd63b4db9d143110b
Dan Seminara <semi225599@gmail.com>
2024-04-11 17:23:36 -0400

    Clean up formatting a bit

--- src/year2015/day21.rs
+++ src/year2015/day21.rs
@@ -33,7 +33,7 @@ struct Person {
 #[rustfmt::skip]
 static SHOP1: Lazy<Vec<Equip>> = Lazy::new(|| {
     vec![
-        Equip { cost: 8, damage: 4, armor: 0 }, // Dagger
+        Equip { cost: 8, damage: 4, armor: 0 },  // Dagger
         Equip { cost: 10, damage: 5, armor: 0 }, // Shortsword
         Equip { cost: 25, damage: 6, armor: 0 }, // Warhammer
         Equip { cost: 40, damage: 7, armor: 0 }, // Longsword
@@ -43,23 +43,23 @@ static SHOP1: Lazy<Vec<Equip>> = Lazy::new(|| {
 #[rustfmt::skip]
 static SHOP2: Lazy<Vec<Equip>> = Lazy::new(|| {
     vec![
-        Equip { cost: 13, damage: 0, armor: 1 }, // Leather
-        Equip { cost: 31, damage: 0, armor: 2 }, // Chainmail
-        Equip { cost: 53, damage: 0, armor: 3 }, // Splintmail
-        Equip { cost: 75, damage: 0, armor: 4 }, // Bandedmail
+        Equip { cost: 13, damage: 0, armor: 1 },  // Leather
+        Equip { cost: 31, damage: 0, armor: 2 },  // Chainmail
+        Equip { cost: 53, damage: 0, armor: 3 },  // Splintmail
+        Equip { cost: 75, damage: 0, armor: 4 },  // Bandedmail
         Equip { cost: 102, damage: 0, armor: 5 }, // Platemail
     ]}
 );
 #[rustfmt::skip]
 static SHOP3: Lazy<Vec<Equip>> = Lazy::new(|| {
     vec![
-        Equip { cost: 25, damage: 1, armor: 0 }, // Damage +1
-        Equip { cost: 50, damage: 2, armor: 0 }, // Damage +2
+        Equip { cost: 25, damage: 1, armor: 0 },  // Damage +1
+        Equip { cost: 50, damage: 2, armor: 0 },  // Damage +2
         Equip { cost: 100, damage: 3, armor: 0 }, // Damage +3
-        Equip { cost: 20, damage: 0, armor: 1 }, // Defense +1
-        Equip { cost: 40, damage: 0, armor: 2 }, // Defense +2
-        Equip { cost: 80, damage: 0, armor: 3 }, // Defense +3
-        Equip { cost: 0, damage: 0, armor: 0 }, // None
+        Equip { cost: 20, damage: 0, armor: 1 },  // Defense +1
+        Equip { cost: 40, damage: 0, armor: 2 },  // Defense +2
+        Equip { cost: 80, damage: 0, armor: 3 },  // Defense +3
+        Equip { cost: 0, damage: 0, armor: 0 },   // None
     ]}
 );
 static ALL_EQUIP_COMBOS: Lazy<Vec<Person>> = Lazy::new(|| {
