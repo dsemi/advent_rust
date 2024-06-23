d3c22d6e7a3bbc24c8b6d604fb6d48116159d022
Dan Seminara <semi225599@gmail.com>
2024-06-23 12:18:58 -0400

    Update Rust to 1.79, shorten 2018 Day 7

--- src/year2018/day07.rs
+++ src/year2018/day07.rs
@@ -1,70 +1,51 @@
 use crate::utils::parsers::*;
-use ahash::{AHashMap, AHashSet};
-use std::cmp::Reverse;
+use crate::utils::*;
 use std::collections::BinaryHeap;
 
-struct Scheduler {
-    avail: BinaryHeap<Reverse<char>>,
-    preds: AHashMap<char, AHashSet<char>>,
-    succs: AHashMap<char, AHashSet<char>>,
-    workers: usize,
-}
-
-fn parse_steps(input: &str, workers: usize) -> Scheduler {
-    let mut preds = AHashMap::new();
-    let mut succs = AHashMap::new();
+fn solve(input: &str, workers: usize) -> (String, i32) {
+    let mut conns = [(0, 0); 26];
     for line in input.lines() {
         let (a, b) = (
             preceded("Step ", any),
             delimited(" must be finished before step ", any, " can begin."),
         )
             .read(line);
-        preds.entry(b).or_insert_with(AHashSet::new).insert(a);
-        succs.entry(a).or_insert_with(AHashSet::new).insert(b);
+        let (a, b) = (b'Z' - a as u8, b'Z' - b as u8);
+        conns[b as usize].0 |= 1 << a;
+        conns[a as usize].1 |= 1 << b;
     }
-    Scheduler {
-        avail: succs
-            .keys()
-            .filter_map(|c| (!preds.contains_key(c)).then_some(Reverse(*c)))
-            .collect(),
-        preds,
-        succs,
-        workers,
-    }
-}
-
-impl Scheduler {
-    fn run(&mut self) -> (String, u32) {
-        let mut done = AHashSet::new();
-        let mut work_queue = BinaryHeap::new();
-        let mut result = ("".to_owned(), 0);
-
-        loop {
-            while !self.avail.is_empty() && work_queue.len() < self.workers {
-                let Reverse(c) = self.avail.pop().unwrap();
-                work_queue.push((Reverse(result.1 + c as u32 - 4), Reverse(c)));
-            }
-            if let Some((Reverse(time), Reverse(curr))) = work_queue.pop() {
-                result.0.push(curr);
-                result.1 = time;
-                done.insert(curr);
-                for st in self.succs.get(&curr).unwrap_or(&AHashSet::new()) {
-                    if done.is_superset(&self.preds[st]) {
-                        self.avail.push(Reverse(*st));
-                    }
-                }
-            } else {
-                break;
+    let mut avail = conns
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &(inc, out))| (inc == 0 && out != 0).then_some(i))
+        .collect::<BinaryHeap<_>>();
+    let mut work_queue = BinaryHeap::new();
+    let mut result = ("".to_owned(), 0);
+    let fill = |avail: &mut BinaryHeap<usize>, q: &mut BinaryHeap<(i32, usize)>, t: i32| {
+        while !avail.is_empty() && q.len() < workers {
+            let c = avail.pop().unwrap();
+            q.push((t - 86 + c as i32, c));
+        }
+    };
+    fill(&mut avail, &mut work_queue, 0);
+    while let Some((time, curr)) = work_queue.pop() {
+        result.0.push((b'Z' - curr as u8) as char);
+        result.1 = -time;
+        for st in bits(conns[curr].1) {
+            conns[st].0 &= !(1 << curr);
+            if conns[st].0 == 0 {
+                avail.push(st);
             }
         }
-        result
+        fill(&mut avail, &mut work_queue, -result.1);
     }
+    result
 }
 
 pub fn part1(input: &str) -> String {
-    parse_steps(input, 1).run().0
+    solve(input, 1).0
 }
 
-pub fn part2(input: &str) -> u32 {
-    parse_steps(input, 5).run().1
+pub fn part2(input: &str) -> i32 {
+    solve(input, 5).1
 }
