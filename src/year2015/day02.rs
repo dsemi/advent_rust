64c6850b6f10a150d8e35b663aca0785ca75f7bb
Dan Seminara <semi225599@gmail.com>
2024-07-07 01:17:38 -0400

    Chain min

--- src/year2015/day02.rs
+++ src/year2015/day02.rs
@@ -1,20 +1,17 @@
 use crate::utils::parsers::*;
 
 fn process(input: &str, f: fn((i32, i32, i32)) -> i32) -> i32 {
-    input
-        .lines()
-        .map(|line| f(sep3(i32, 'x').read(line)))
-        .sum()
+    input.lines().map(|line| f(sep3(i32, 'x').read(line))).sum()
 }
 
 pub fn part1(input: &str) -> i32 {
     process(input, |(l, w, h)| {
-        2 * (l * w + l * h + w * h) + [l * w, l * h, w * h].iter().min().unwrap()
+        2 * (l * w + l * h + w * h) + (l * w).min(l * h).min(w * h)
     })
 }
 
 pub fn part2(input: &str) -> i32 {
     process(input, |(l, w, h)| {
-        l * w * h + 2 * [l + w, l + h, w + h].iter().min().unwrap()
+        l * w * h + 2 * (l + w).min(l + h).min(w + h)
     })
 }
