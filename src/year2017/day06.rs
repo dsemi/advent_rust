c868c9bab3e97322cffed71de07e1d7244c8d267
Dan Seminara <semi225599@gmail.com>
2024-09-28 22:17:40 -0400

    Speed up 2017 day 6

--- src/year2017/day06.rs
+++ src/year2017/day06.rs
@@ -1,25 +1,62 @@
-use ahash::AHashMap;
-use std::cmp::Reverse;
+const REMOVE: u128 = 0x00ffffffffffffffffffffffffffffff;
+const REDIST: [u128; 17] = [
+    0x00000000000000000000000000000000,
+    0x00010000000000000000000000000000,
+    0x00010100000000000000000000000000,
+    0x00010101000000000000000000000000,
+    0x00010101010000000000000000000000,
+    0x00010101010100000000000000000000,
+    0x00010101010101000000000000000000,
+    0x00010101010101010000000000000000,
+    0x00010101010101010100000000000000,
+    0x00010101010101010101000000000000,
+    0x00010101010101010101010000000000,
+    0x00010101010101010101010100000000,
+    0x00010101010101010101010101000000,
+    0x00010101010101010101010101010000,
+    0x00010101010101010101010101010100,
+    0x00010101010101010101010101010101,
+    0x01010101010101010101010101010101,
+];
+const MASK: u128 = 0x10101010101010101010101010101010;
 
-fn redistribute_until_cycle(mut ns: Vec<usize>) -> (usize, usize) {
-    let len = ns.len();
-    let mut m: AHashMap<Vec<usize>, usize> = AHashMap::new();
-    for c in 0.. {
-        if m.contains_key(&ns) {
-            return (c, c - m[&ns]);
-        }
-        m.insert(ns.clone(), c);
-        let (j, &val) = ns
-            .iter()
-            .enumerate()
-            .max_by_key(|&(i, x)| (x, Reverse(i)))
-            .unwrap();
-        ns[j] = 0;
-        for k in j + 1..=j + val {
-            ns[k % len] += 1;
-        }
+fn init(ns: Vec<usize>) -> u128 {
+    ns.into_iter().fold(0, |acc, n| (acc << 8) + n as u128)
+}
+
+fn next(mem: u128) -> u128 {
+    let mut mask = MASK;
+    for n in 0..5 {
+        let m = (mem << n) & mask;
+        mask = if m == 0 { mask } else { m };
     }
-    (0, 0)
+    let offset = mask.leading_zeros() - 3;
+    let max = (mem.rotate_left(offset + 8) & 0xff) as usize;
+    (mem & REMOVE.rotate_right(offset)) + REDIST[max].rotate_right(offset)
+}
+
+fn redistribute_until_cycle(ns: Vec<usize>) -> (usize, usize) {
+    let mem = init(ns);
+    let mut h0 = next(mem);
+    let mut h1 = next(next(mem));
+    while h0 != h1 {
+        h0 = next(h0);
+        h1 = next(next(h1));
+    }
+    let mut mu = 0;
+    let mut h0 = mem;
+    while h0 != h1 {
+        h0 = next(h0);
+        h1 = next(h1);
+        mu += 1;
+    }
+    let mut lambda = 1;
+    let mut h1 = next(h0);
+    while h0 != h1 {
+        h1 = next(h1);
+        lambda += 1;
+    }
+    (mu + lambda, lambda)
 }
 
 pub fn part1(input: Vec<usize>) -> usize {
