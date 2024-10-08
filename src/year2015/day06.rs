0aae12dbbec3adb90570237ecc0fcf4003296430
Dan Seminara <semi225599@gmail.com>
2024-10-07 21:36:05 -0400

    Use ndarray for 2015 day 6

--- src/year2015/day06.rs
+++ src/year2015/day06.rs
@@ -1,49 +1,48 @@
 use crate::utils::parsers::*;
+use ndarray::{s, Array2, ArrayViewMut2};
+use Cmd::*;
 
-type Switch = fn(&mut [u32]);
-type Coord = (usize, usize);
-
-fn parse<'a>(
-    off: Switch,
-    on: Switch,
-    toggle: Switch,
-) -> impl Parser<&'a str, (Switch, (Coord, Coord)), ContextError> {
-    (
-        alt((
-            "turn off ".value(off),
-            "turn on ".value(on),
-            "toggle ".value(toggle),
-        )),
-        sep2(coord(usize), "through"),
-    )
+#[derive(Clone)]
+enum Cmd {
+    On,
+    Toggle,
+    Off,
 }
 
-fn run_commands(input: &str, off: Switch, on: Switch, toggle: Switch) -> u32 {
-    let mut grid = vec![0; 1000000];
+fn instr(i: &mut &str) -> PResult<(Cmd, usize, usize, usize, usize)> {
+    let cmd = alt((
+        "turn on ".value(On),
+        "toggle ".value(Toggle),
+        "turn off ".value(Off),
+    ))
+    .parse_next(i)?;
+    let ((x0, y0), (x1, y1)) = sep2(coord(usize), "through").parse_next(i)?;
+    Ok((cmd, x0, x1, y0, y1))
+}
+
+// Use Saturating if it gets included in num_traits::Zero.
+fn run_commands(input: &str, f: fn(Cmd, ArrayViewMut2<u32>)) -> u32 {
+    let mut grid = Array2::zeros((1000, 1000));
     for line in input.lines() {
-        let (f, ((x0, y0), (x1, y1))) = parse(off, on, toggle).read(line);
-        for x in x0..=x1 {
-            let row = 1000 * x;
-            f(&mut grid[row + y0..=row + y1]);
-        }
+        let (cmd, x0, x1, y0, y1) = instr.read(line);
+        let slice = grid.slice_mut(s![x0..=x1, y0..=y1]);
+        f(cmd, slice);
     }
-    grid.into_iter().sum()
+    grid.sum()
 }
 
 pub fn part1(input: &str) -> u32 {
-    run_commands(
-        input,
-        |s| s.fill(0),
-        |s| s.fill(1),
-        |s| s.iter_mut().for_each(|v| *v ^= 1),
-    )
+    run_commands(input, |cmd, mut slice| match cmd {
+        On => slice |= 1,
+        Toggle => slice ^= 1,
+        Off => slice &= 0,
+    })
 }
 
 pub fn part2(input: &str) -> u32 {
-    run_commands(
-        input,
-        |s| s.iter_mut().for_each(|v| *v = v.saturating_sub(1)),
-        |s| s.iter_mut().for_each(|v| *v += 1),
-        |s| s.iter_mut().for_each(|v| *v += 2),
-    )
+    run_commands(input, |cmd, mut slice| match cmd {
+        On => slice += 1,
+        Toggle => slice += 2,
+        Off => slice.mapv_inplace(|v: u32| v.saturating_sub(1)),
+    })
 }
