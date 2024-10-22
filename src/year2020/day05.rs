d3f36bf74ba48f24ec5dc8ee2ef507b382db69fb
Dan Seminara <semi225599@gmail.com>
2024-10-22 13:24:47 -0400

    Simplify

--- src/year2020/day05.rs
+++ src/year2020/day05.rs
@@ -1,23 +1,18 @@
-fn seat_ids(s: &str) -> Vec<i64> {
-    s.chars()
-        .map(|x| match x {
-            'F' | 'L' => '0',
-            'B' | 'R' => '1',
-            c => c,
-        })
-        .collect::<String>()
-        .lines()
-        .map(|line| i64::from_str_radix(line, 2).unwrap())
-        .collect()
+fn seat_ids(s: &str) -> impl Iterator<Item = i64> + use<'_> {
+    s.lines().map(|line| {
+        line.bytes()
+            .map(|b| (b & 0b100) >> 2 ^ 1)
+            .fold(0, |acc, d| acc << 1 | d as i64)
+    })
 }
 
 pub fn part1(input: &str) -> Option<i64> {
-    seat_ids(input).into_iter().max()
+    seat_ids(input).max()
 }
 
 pub fn part2(input: &str) -> Option<i64> {
-    let mut ids = seat_ids(input);
+    let mut ids: Vec<_> = seat_ids(input).collect();
     ids.sort_unstable();
     ids.windows(2)
-        .find_map(|wind| (wind[0] + 2 == wind[1]).then_some(wind[0] + 1))
+        .find_map(|w| (w[0] + 2 == w[1]).then_some(w[0] + 1))
 }
