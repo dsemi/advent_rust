d6865ccc6392d3b5eade59f272b397583d4d9d2e
Dan Seminara <semi225599@gmail.com>
2024-12-02 01:47:09 -0500

    Combinations

--- src/year2024/day02.rs
+++ src/year2024/day02.rs
@@ -1,25 +1,23 @@
 use crate::utils::parsers::*;
+use crate::utils::*;
+use std::borrow::Borrow;
+use streaming_iterator::StreamingIterator;
 
-fn valid(ns: &[u8]) -> bool {
-    let ord = ns[0].cmp(&ns[1]);
-    ns.windows(2).all(|w| w[0].cmp(&w[1]) == ord && (1..=3).contains(&w[0].abs_diff(w[1])))
+fn valid<T: Borrow<i8>>(ns: &[T]) -> bool {
+    let sgn = (ns[0].borrow() - ns[1].borrow()).signum();
+    ns.windows(2)
+        .map(|w| w[0].borrow() - w[1].borrow())
+        .all(|d| d.signum() == sgn && (1..=3).contains(&d.abs()))
 }
 
 pub fn part1(input: &str) -> usize {
-    input.lines().map(|line| spaced(u8).read(line)).filter(|ns| valid(ns)).count()
+    input.lines().map(|line| spaced(i8).read(line)).filter(|ns| valid(ns)).count()
 }
 
 pub fn part2(input: &str) -> usize {
     input
         .lines()
-        .map(|line| spaced(u8).read(line))
-        .filter(|ns| {
-            valid(ns)
-                || (0..ns.len()).any(|i| {
-                    let mut ns = ns.clone();
-                    ns.remove(i);
-                    valid(&ns)
-                })
-        })
+        .map(|line| spaced(i8).read(line))
+        .filter(|ns| valid(ns) || Combinations::new(&ns, ns.len() - 1).any(|ns| valid(ns)))
         .count()
 }
