b9800687018930b6420768c958cc1ee070882925
Dan Seminara <semi225599@gmail.com>
2024-12-05 09:58:44 -0500

    Switch to array and use select_nth_unstable_by

--- src/year2024/day05.rs
+++ src/year2024/day05.rs
@@ -1,32 +1,32 @@
-use ahash::{AHashMap, AHashSet};
-use std::cmp::Ordering::*;
+use std::cmp::Ordering::{self, *};
 
 use crate::utils::parsers::*;
 
-fn parse(input: &str) -> (AHashMap<u32, AHashSet<u32>>, impl Iterator<Item = Vec<u32>> + use<'_>) {
+fn parse(input: &str) -> ([[Ordering; 100]; 100], impl Iterator<Item = Vec<usize>> + use<'_>) {
     let (rules, pages) = input.split_once("\n\n").unwrap();
-    let mut rule_map = AHashMap::new();
-    for (k, v) in rules.lines().map(|line| sep2(u32, '|').read(line)) {
-        rule_map.entry(k).or_insert_with(AHashSet::new).insert(v);
+    let mut rule_map = [[Equal; 100]; 100];
+    for (a, b) in rules.lines().map(|line| sep2(usize, '|').read(line)) {
+        rule_map[a][b] = Less;
+        rule_map[b][a] = Greater;
     }
-    (rule_map, pages.lines().map(|line| list(u32).read(line)))
+    (rule_map, pages.lines().map(|line| list(usize).read(line)))
 }
 
-pub fn part1(input: &str) -> u32 {
+pub fn part1(input: &str) -> usize {
     let (rules, pages) = parse(input);
     pages
-        .filter(|page| page.is_sorted_by(|a, b| rules[a].contains(b)))
+        .filter(|page| page.is_sorted_by(|&a, &b| rules[a][b] == Less))
         .map(|page| page[page.len() / 2])
         .sum()
 }
 
-pub fn part2(input: &str) -> u32 {
+pub fn part2(input: &str) -> usize {
     let (rules, pages) = parse(input);
     pages
-        .filter(|page| !page.is_sorted_by(|a, b| rules[a].contains(b)))
+        .filter(|page| !page.is_sorted_by(|&a, &b| rules[a][b] == Less))
         .map(|mut page| {
-            page.sort_by(|a, b| if rules[a].contains(b) { Less } else { Greater });
-            page[page.len() / 2]
+            let middle = page.len() / 2;
+            *page.select_nth_unstable_by(middle, |&a, &b| rules[a][b]).1
         })
         .sum()
 }
