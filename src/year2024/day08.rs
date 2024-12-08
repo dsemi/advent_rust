169cce148107b05f11618b317b5e3e25817536b8
Dan Seminara <semi225599@gmail.com>
2024-12-08 00:55:16 -0500

    2024 Day 8

---
+++ src/year2024/day08.rs
@@ -0,0 +1,34 @@
+use crate::utils::*;
+use ahash::{AHashMap, AHashSet};
+use itertools::iterate;
+use std::iter::once;
+use streaming_iterator::StreamingIterator;
+
+fn solve<I, I2>(input: &str, pts: fn(C<i32>, C<i32>) -> (I, I2)) -> usize
+where
+    I: Iterator<Item = C<i32>>,
+    I2: Iterator<Item = C<i32>>,
+{
+    let grid: Grid<u8, i32> = input.bytes().collect();
+    let mut groups = AHashMap::new();
+    grid.idx_iter()
+        .filter(|&(_, &k)| k != b'.')
+        .for_each(|(v, &k)| groups.entry(k).or_insert_with(Vec::new).push(v));
+    let mut anti_nodes = AHashSet::new();
+    groups.values().filter(|g| g.len() >= 2).for_each(|group| {
+        group.combinations(2).for_each(|combo| {
+            let (a_s, b_s) = pts(*combo[0], *combo[1]);
+            anti_nodes.extend(a_s.take_while(|&i| grid.in_bounds(i)));
+            anti_nodes.extend(b_s.take_while(|&i| grid.in_bounds(i)));
+        })
+    });
+    anti_nodes.len()
+}
+
+pub fn part1(input: &str) -> usize {
+    solve(input, |a, b| (once(a + a - b), once(b + b - a)))
+}
+
+pub fn part2(input: &str) -> usize {
+    solve(input, |a, b| (iterate(a, move |v| v + a - b), iterate(b, move |v| v + b - a)))
+}
