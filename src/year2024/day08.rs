299be62d1e1bc8eba26d0d01d9d5b06c1b34b554
Dan Seminara <semi225599@gmail.com>
2024-12-08 01:29:54 -0500

    Don't need combinations

--- src/year2024/day08.rs
+++ src/year2024/day08.rs
@@ -2,7 +2,6 @@ use crate::utils::*;
 use ahash::{AHashMap, AHashSet};
 use itertools::iterate;
 use std::iter::once;
-use streaming_iterator::StreamingIterator;
 
 fn solve<I, I2>(input: &str, pts: fn(C<i32>, C<i32>) -> (I, I2)) -> usize
 where
@@ -11,17 +10,16 @@ where
 {
     let grid: Grid<u8, i32> = input.bytes().collect();
     let mut groups = AHashMap::new();
-    grid.idx_iter()
-        .filter(|&(_, &k)| k != b'.')
-        .for_each(|(v, &k)| groups.entry(k).or_insert_with(Vec::new).push(v));
     let mut anti_nodes = AHashSet::new();
-    groups.values().filter(|g| g.len() >= 2).for_each(|group| {
-        group.combinations(2).for_each(|combo| {
-            let (a_s, b_s) = pts(*combo[0], *combo[1]);
+    for (b, &v) in grid.idx_iter().filter(|&(_, &v)| v != b'.') {
+        let e = groups.entry(v).or_insert_with(Vec::new);
+        for &a in e.iter() {
+            let (a_s, b_s) = pts(a, b);
             anti_nodes.extend(a_s.take_while(|&i| grid.in_bounds(i)));
             anti_nodes.extend(b_s.take_while(|&i| grid.in_bounds(i)));
-        })
-    });
+        }
+        e.push(b);
+    }
     anti_nodes.len()
 }
 
