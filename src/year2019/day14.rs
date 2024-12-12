014d60921399b5bc71d938ee6dc2b1ad1c7c5e27
Dan Seminara <semi225599@gmail.com>
2024-12-11 23:16:30 -0500

    Remove ahash

--- src/year2019/day14.rs
+++ src/year2019/day14.rs
@@ -1,11 +1,10 @@
 use crate::utils::parsers::*;
 use crate::utils::partition_point;
-use ahash::AHashMap;
-use genawaiter;
+use hashbrown::HashMap;
 
 struct Reactions<'a> {
     #[expect(clippy::type_complexity)]
-    graph: AHashMap<&'a str, (i64, Vec<(i64, &'a str)>)>,
+    graph: HashMap<&'a str, (i64, Vec<(i64, &'a str)>)>,
     topo: Vec<&'a str>,
 }
 
@@ -21,10 +20,10 @@ fn parse<'a>(i: &mut &'a str) -> PResult<(&'a str, (i64, Vec<(i64, &'a str)>))> 
 }
 
 fn parse_reactions(input: &str) -> Reactions {
-    let graph = lines_iter(input, parse).collect::<AHashMap<_, _>>();
-    let mut incoming = AHashMap::new();
+    let graph = lines_iter(input, parse).collect::<HashMap<_, _>>();
+    let mut incoming = HashMap::new();
     graph.values().for_each(|(_, srcs)| {
-        srcs.iter().for_each(|(_, src)| *incoming.entry(src).or_insert(0) += 1)
+        srcs.iter().for_each(|&(_, src)| *incoming.entry(src).or_insert(0) += 1)
     });
     let mut topo = Vec::new();
     let mut no_incoming = vec!["FUEL"];
@@ -44,7 +43,7 @@ fn parse_reactions(input: &str) -> Reactions {
 }
 
 fn num_ore(reactions: &Reactions, n: i64) -> i64 {
-    let mut cnts: AHashMap<&str, i64> = vec![("FUEL", n)].into_iter().collect();
+    let mut cnts: HashMap<&str, i64> = vec![("FUEL", n)].into_iter().collect();
     reactions.topo.iter().for_each(|e| {
         let (n, srcs) = &reactions.graph[e];
         // Num reactions required to produce n of e.
