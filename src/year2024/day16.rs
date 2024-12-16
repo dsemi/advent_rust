882dbad41009b4924da1cdb1dd8e099bad9db973
Dan Seminara <semi225599@gmail.com>
2024-12-16 10:43:22 -0500

    Remove excess hashing

--- src/year2024/day16.rs
+++ src/year2024/day16.rs
@@ -1,51 +1,50 @@
 use crate::utils::*;
-use hashbrown::{HashMap, HashSet};
 
 type PosDir = (C<i32>, C<i32>);
 
-fn neighbors(
-    parents: &mut HashMap<PosDir, Vec<PosDir>>,
-    grid: &Grid<u8, i32>,
-    (pos, dir): PosDir,
-) -> Vec<(usize, PosDir)> {
+fn neighbors(grid: &Grid<u8, i32>, (pos, dir): PosDir) -> Vec<(usize, PosDir)> {
     [(1, dir), (1001, dir * C(0, 1)), (1001, dir * C(0, -1))]
         .into_iter()
         .map(|(dist, dir)| (dist, (pos + dir, dir)))
         .filter(|(_, (pos, _))| grid[*pos] != b'#')
-        .inspect(|&(_, n)| parents.entry(n).or_insert_with(Vec::new).push((pos, dir)))
         .collect()
 }
 
 pub fn part1(input: &str) -> Option<usize> {
     let grid: Grid<u8, i32> = input.bytes().collect();
     let start = (grid.position(|&v| v == b'S').unwrap(), C(0, 1));
-    let mut parents = HashMap::new();
-    dijkstra(start, |&posdir| neighbors(&mut parents, &grid, posdir))
+    dijkstra(start, |&posdir| neighbors(&grid, posdir))
         .find_map(|(score, (pos, _))| (grid[pos] == b'E').then_some(score))
+}
+
+fn idx(C(a, b): C<i32>) -> usize {
+    (((a as u8 & 3).div_ceil(2) << 2) | (b as u8 & 3).div_ceil(2)).leading_zeros() as usize - 4
 }
 
 pub fn part2(input: &str) -> usize {
     let grid: Grid<u8, i32> = input.bytes().collect();
     let start = (grid.position(|&v| v == b'S').unwrap(), C(0, 1));
-    let mut parents = HashMap::new();
-    let mut scores = HashMap::new();
-    scores.insert(start, 0);
-    let (score, k) = dijkstra(start, |&posdir| neighbors(&mut parents, &grid, posdir))
-        .find(|&(score, k)| {
-            scores.entry(k).or_insert(score);
-            grid[k.0] == b'E'
+    let mut scores = grid.same_size_with([usize::MAX; 4]);
+    scores[start.0][idx(start.1)] = 0;
+    let (score, k) = dijkstra(start, |&posdir| neighbors(&grid, posdir))
+        .find(|&(score, (pos, dir))| {
+            scores[pos][idx(dir)] = scores[pos][idx(dir)].min(score);
+            grid[pos] == b'E'
         })
         .unwrap();
-    let mut sqs = HashSet::new();
+    let mut sqs = grid.same_size_with(false);
     let mut stack = vec![(k, score)];
-    while let Some((k, score)) = stack.pop() {
-        sqs.insert(k.0);
-        parents
-            .get(&k)
-            .unwrap_or(&vec![])
-            .iter()
-            .filter(|&p| scores[p] == score - 1 || scores[p] == score - 1001)
-            .for_each(|p| stack.push((*p, scores[p])));
+    while let Some(((pos, dir), score)) = stack.pop() {
+        sqs[pos] = true;
+        [
+            ((pos - dir, dir), score - 1),
+            ((pos - dir, dir * C(0, 1)), score - 1001),
+            ((pos - dir, dir * C(0, -1)), score - 1001),
+        ]
+        .into_iter()
+        .filter(|((pos, _), _)| grid[*pos] != b'#')
+        .filter(|&((pos, dir), s)| scores[pos][idx(dir)] == s)
+        .for_each(|e| stack.push(e));
     }
-    sqs.len()
+    sqs.into_iter().filter(|&b| b).count()
 }
