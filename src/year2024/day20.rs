02e49de6a37071397c9a77ab57757cd68577a190
Dan Seminara <semi225599@gmail.com>
2024-12-20 12:52:40 -0500

    Parallelize

--- src/year2024/day20.rs
+++ src/year2024/day20.rs
@@ -1,28 +1,34 @@
 use crate::utils::*;
+use rayon::prelude::*;
 
-fn solve(input: &str, cheat_time: i32) -> usize {
+fn solve(input: &str, cheat_time: usize) -> usize {
     let grid: Grid<u8, i32> = input.bytes().collect();
-    let start = grid.position(|&v| v == b'S').unwrap();
+    let mut prev = grid.position(|&v| v == b'S').unwrap();
     let mut times = grid.same_size_with(0);
-    std::iter::successors(Some((start, start, 1)), |&(prev, p, time)| {
+    let path: Vec<_> = std::iter::successors(Some(prev), |&p| {
+        let prev = std::mem::replace(&mut prev, p);
         [C(p.0, p.1 - 1), C(p.0, p.1 + 1), C(p.0 + 1, p.1), C(p.0 - 1, p.1)]
             .into_iter()
             .find(|&pos| pos != prev && grid[pos] != b'#')
-            .map(|pos| (p, pos, time + 1))
     })
-    .for_each(|(_, p, time)| times[p] = time);
-    let mut total = 0;
-    for (p, &t) in times.idx_iter().filter(|&(_, &t)| t > 0) {
-        let f = |d| (*times.get(p + d).unwrap_or(&0) >= t + C(0, 0).dist(&d) + 100) as usize;
-        (2..=cheat_time).for_each(|c| total += f(C(0, c)) + f(C(0, -c)));
-        for r in 1..=cheat_time {
-            total += f(C(r, 0)) + f(C(-r, 0));
-            for c in 1..=cheat_time - r {
-                total += f(C(r, c)) + f(C(r, -c)) + f(C(-r, c)) + f(C(-r, -c));
-            }
-        }
-    }
-    total
+    .collect();
+    path.iter().zip(1..).for_each(|(&p, time)| times[p] = time);
+    path[..path.len() - cheat_time - 1]
+        .par_iter()
+        .enumerate()
+        .map(|(t, p)| {
+            let t = t as i32 + 1;
+            let f = |d| (*times.get(p + d).unwrap_or(&0) >= t + C(0, 0).dist(&d) + 100) as usize;
+            (1..=cheat_time as i32)
+                .map(|r| {
+                    let n = f(C(r, 0)) + f(C(-r, 0)) + f(C(0, r)) + f(C(0, -r));
+                    n + (1..=cheat_time as i32 - r)
+                        .map(|c| f(C(r, c)) + f(C(r, -c)) + f(C(-r, c)) + f(C(-r, -c)))
+                        .sum::<usize>()
+                })
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 pub fn part1(input: &str) -> usize {
