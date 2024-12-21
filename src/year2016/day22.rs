3138a195d91c18f4c81cd7d0be4d60a2703427c4
Dan Seminara <semi225599@gmail.com>
2024-12-21 14:29:46 -0500

    Shorten

--- src/year2016/day22.rs
+++ src/year2016/day22.rs
@@ -1,66 +1,46 @@
 use crate::utils::parsers::*;
 use crate::utils::*;
-use hashbrown::HashMap;
-use std::cmp::max;
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 struct Node {
-    coord: C<i32>,
     used: i64,
     avail: i64,
 }
 
-fn node(i: &mut &str) -> PResult<Node> {
+fn node(i: &mut &str) -> PResult<(C<i32>, Node)> {
     "/dev/grid/node-x".parse_next(i)?;
     let coord = sep2(i32, "-y").output_into().parse_next(i)?;
     delimited(space1, i64, 'T').parse_next(i)?;
     let used = delimited(space1, i64, 'T').parse_next(i)?;
     let avail = delimited(space1, i64, 'T').parse_next(i)?;
     delimited(space1, i64, '%').parse_next(i)?;
-    Ok(Node { coord, used, avail })
-}
-
-fn parse_nodes(input: &str) -> Vec<Node> {
-    input.lines().skip(2).map(|line| node.read(line)).collect()
+    Ok((coord, Node { used, avail }))
 }
 
 pub fn part1(input: &str) -> usize {
-    let nodes = parse_nodes(input);
-    (0..nodes.len())
-        .map(|i| {
-            (i + 1..nodes.len())
-                .filter(|&j| {
-                    nodes[i].used > 0 && nodes[i].used < nodes[j].avail
-                        || nodes[j].used > 0 && nodes[j].used < nodes[i].avail
-                })
-                .count()
-        })
-        .sum()
+    let nodes: Vec<Node> = input.lines().skip(2).map(|line| node.read(line).1).collect();
+    let mut total = 0;
+    for (i, a) in nodes.iter().enumerate() {
+        for b in nodes.iter().skip(i + 1) {
+            total += ((1..b.avail).contains(&a.used) || (1..a.avail).contains(&b.used)) as usize;
+        }
+    }
+    total
 }
 
-fn neighbors(grid: &HashMap<C<i32>, Node>, st: &(C<i32>, C<i32>)) -> Vec<(C<i32>, C<i32>)> {
-    vec![C(0, 1), C(0, -1), C(1, 0), C(-1, 0)]
+fn neighbors(grid: &Grid<i64, i32>, &(opn, goal): &(C<i32>, C<i32>)) -> Vec<(C<i32>, C<i32>)> {
+    vec![opn + C(0, 1), opn + C(0, -1), opn + C(1, 0), opn + C(-1, 0)]
         .into_iter()
-        .filter_map(move |d| {
-            let o2 = st.0 + d;
-            (grid.contains_key(&o2) && grid[&o2].used <= 100)
-                .then(|| (o2, if o2 == st.1 { st.0 } else { st.1 }))
-        })
+        .filter(|&o2| *grid.get(o2).unwrap_or(&i64::MAX) <= 100)
+        .map(|o2| (o2, if o2 == goal { opn } else { goal }))
         .collect()
 }
 
 pub fn part2(input: &str) -> Option<usize> {
-    let nodes = parse_nodes(input);
-    let mut grid = HashMap::new();
-    let mut opn = C(0, 0);
-    let mut mx = 0;
-    for node in nodes {
-        grid.insert(node.coord, node.clone());
-        if node.used == 0 {
-            opn = node.coord;
-        }
-        mx = max(mx, node.coord.0);
-    }
-    bfs((opn, C(mx, 0)), move |st| neighbors(&grid, st))
-        .find_map(|(d, v)| (v.1 == C(0, 0)).then_some(d))
+    let max_coord = node.read(input.lines().last().unwrap()).0;
+    let mut grid = Grid::new_with(max_coord.0 + 1, max_coord.1 + 1, 0);
+    input.lines().skip(2).map(|line| node.read(line)).for_each(|(i, node)| grid[i] = node.used);
+    let opn = grid.position(|&used| used == 0).unwrap();
+    let mx = C(grid.rows - 1, 0);
+    bfs2((opn, mx), move |st| neighbors(&grid, st)).find_map(|(d, v)| (v.1 == C(0, 0)).then_some(d))
 }
