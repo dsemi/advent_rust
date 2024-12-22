0afe566566945cebe1e995712b53a20b43f9aafc
Dan Seminara <semi225599@gmail.com>
2024-12-22 01:48:35 -0500

    Speed up

--- src/year2024/day22.rs
+++ src/year2024/day22.rs
@@ -2,36 +2,38 @@ use crate::utils::parsers::*;
 use hashbrown::HashMap;
 use itertools::{iterate, Itertools};
 use rayon::prelude::*;
+use std::iter::zip;
+
+const MASK: i64 = (1 << 24) - 1;
 
 fn step(secret: &i64) -> i64 {
-    let mut secret = (secret ^ secret << 6) % 16777216;
-    secret = (secret ^ secret >> 5) % 16777216;
-    (secret ^ secret << 11) % 16777216
+    let mut secret = (secret ^ secret << 6) & MASK;
+    secret ^= secret >> 5;
+    (secret ^ secret << 11) & MASK
 }
 
 pub fn part1(input: &str) -> i64 {
     input.lines().map(|p| iterate(p.i64(), step).nth(2000).unwrap()).sum()
 }
 
+fn idx(a: i64, b: i64, c: i64, d: i64) -> usize {
+    (6859 * (a + 9) + 361 * (b + 9) + 19 * (c + 9) + d + 9) as usize
+}
+
 pub fn part2(input: &str) -> i64 {
     input
         .par_lines()
         .map(|p| {
+            let mut dp = vec![0; 19usize.pow(4)];
             let ns: Vec<_> = iterate(p.i64(), step).take(2001).map(|n| n % 10).collect();
             ns.into_iter()
                 .rev()
                 .tuple_windows()
-                .map(|(a, b, c, d, e)| ((d - e, c - d, b - c, a - b), a))
-                .collect::<HashMap<_, _>>()
+                .for_each(|(a, b, c, d, e)| dp[idx(d - e, c - d, b - c, a - b)] = a);
+            dp
         })
-        .reduce(
-            || HashMap::new(),
-            |mut acc, m| {
-                m.into_iter().for_each(|(k, v)| *acc.entry(k).or_insert(0) += v);
-                acc
-            },
-        )
-        .into_values()
+        .reduce(|| vec![0; 19usize.pow(4)], |a, b| zip(a, b).map(|(a, b)| a + b).collect())
+        .into_iter()
         .max()
         .unwrap()
 }
