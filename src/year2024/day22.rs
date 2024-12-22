9e0e3c989f86e0aa4092adacc5ac53588d50d89e
Dan Seminara <semi225599@gmail.com>
2024-12-22 02:02:30 -0500

    Faster

--- src/year2024/day22.rs
+++ src/year2024/day22.rs
@@ -1,11 +1,9 @@
 use crate::utils::parsers::*;
-use hashbrown::HashMap;
 use itertools::{iterate, Itertools};
-use rayon::prelude::*;
-use std::iter::zip;
 
 const MASK: i64 = (1 << 24) - 1;
 
+#[inline]
 fn step(secret: &i64) -> i64 {
     let mut secret = (secret ^ secret << 6) & MASK;
     secret ^= secret >> 5;
@@ -16,24 +14,22 @@ pub fn part1(input: &str) -> i64 {
     input.lines().map(|p| iterate(p.i64(), step).nth(2000).unwrap()).sum()
 }
 
+// Range of -9..=9 possible for each, 19.pow(4) possible values
 fn idx(a: i64, b: i64, c: i64, d: i64) -> usize {
     (6859 * (a + 9) + 361 * (b + 9) + 19 * (c + 9) + d + 9) as usize
 }
 
 pub fn part2(input: &str) -> i64 {
-    input
-        .par_lines()
-        .map(|p| {
-            let mut dp = vec![0; 19usize.pow(4)];
-            let ns: Vec<_> = iterate(p.i64(), step).take(2001).map(|n| n % 10).collect();
-            ns.into_iter()
-                .rev()
-                .tuple_windows()
-                .for_each(|(a, b, c, d, e)| dp[idx(d - e, c - d, b - c, a - b)] = a);
-            dp
-        })
-        .reduce(|| vec![0; 19usize.pow(4)], |a, b| zip(a, b).map(|(a, b)| a + b).collect())
-        .into_iter()
-        .max()
-        .unwrap()
+    let mut dp = vec![0; 19_usize.pow(4)];
+    let mut dpi = vec![0; 19_usize.pow(4)];
+    for (p, i) in input.lines().zip(1..) {
+        for (e, d, c, b, a) in iterate(p.i64(), step).take(2001).map(|n| n % 10).tuple_windows() {
+            let idx = idx(d - e, c - d, b - c, a - b);
+            if dpi[idx] != i {
+                dpi[idx] = i;
+                dp[idx] += a;
+            }
+        }
+    }
+    dp.into_iter().max().unwrap()
 }
