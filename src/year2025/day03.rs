6580be76ed5a4b01f806668e35c467794bf389e7
Dan Seminara <semi225599@gmail.com>
2025-12-03 02:08:38 -0500

    dedup

--- src/year2025/day03.rs
+++ src/year2025/day03.rs
@@ -1,19 +1,19 @@
 use crate::utils::*;
 
-fn max_jolt(row: &[u64], ns: usize) -> u64 {
-    let mut start = 0;
-    (row.len() - ns + 1..=row.len()).fold(0, |curr, end| {
-        start = (start..end).rev().max_by_key(|&i| row[i]).unwrap() + 1;
-        10 * curr + row[start - 1]
+fn solve(input: &str, ns: usize) -> u64 {
+    Grid::<u64>::ints(input.bytes()).rows().fold(0, |sum, row| {
+        let mut start = 0;
+        (row.len() - ns + 1..=row.len()).fold(0, |curr, end| {
+            start = (start..end).rev().max_by_key(|&i| row[i]).unwrap() + 1;
+            10 * curr + row[start - 1]
+        }) + sum
     })
 }
 
 pub fn part1(input: &str) -> u64 {
-    let grid: Grid<u64> = Grid::ints(input.bytes());
-    grid.rows().map(|row| max_jolt(row, 2)).sum()
+    solve(input, 2)
 }
 
 pub fn part2(input: &str) -> u64 {
-    let grid: Grid<u64> = Grid::ints(input.bytes());
-    grid.rows().map(|row| max_jolt(row, 12)).sum()
+    solve(input, 12)
 }
