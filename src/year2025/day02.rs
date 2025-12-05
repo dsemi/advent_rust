c1197c0a390170f6748446f99f12924d5b8d798c
Dan Seminara <semi225599@gmail.com>
2025-12-05 16:58:11 -0500

    generate instead of checking every value

--- src/year2025/day02.rs
+++ src/year2025/day02.rs
@@ -1,25 +1,43 @@
 use crate::utils::parsers::*;
-use rayon::prelude::*;
+use itertools::Itertools;
 
-fn solve(input: &str, invalid: fn(Vec<u8>) -> bool) -> u64 {
-    list(sep2(u64, '-'))
-        .read(input)
-        .into_par_iter()
-        .map(|(lo, hi)| {
-            (lo..=hi).into_par_iter().filter(|n| invalid(n.to_string().into_bytes())).sum::<u64>()
-        })
-        .sum()
+struct Range {
+    lo: u64,
+    hi: u64,
+    step: u64,
 }
+
+const fn calc(digits: u32, seg_size: u32) -> Range {
+    let step = (10_u64.pow(digits) - 1) / (10_u64.pow(seg_size) - 1);
+    let lo = step * 10_u64.pow(seg_size - 1);
+    let hi = step * (10_u64.pow(seg_size) - 1);
+    Range { lo, hi, step }
+}
+
+fn solve(intervals: &[(u64, u64)], ranges: &[Range]) -> u64 {
+    let mut result = 0;
+    for (range, &(lo, hi)) in ranges.iter().cartesian_product(intervals) {
+        let lo = lo.next_multiple_of(range.step).max(range.lo);
+        let hi = hi.min(range.hi);
+        if lo <= hi {
+            let n = (hi - lo) / range.step;
+            result += lo * (n + 1) + range.step * n * (n + 1) / 2;
+        }
+    }
+    result
+}
+
+const HALVES: [Range; 5] = [calc(2, 1), calc(4, 2), calc(6, 3), calc(8, 4), calc(10, 5)];
 
 pub fn part1(input: &str) -> u64 {
-    solve(input, |s| s.len() % 2 == 0 && s[..s.len() / 2] == s[s.len() / 2..])
+    let ints = list(sep2(u64, '-')).read(input);
+    solve(&ints, &HALVES)
 }
 
+const NEW: [Range; 6] = [calc(3, 1), calc(5, 1), calc(6, 2), calc(7, 1), calc(9, 3), calc(10, 2)];
+const OVERLAP: [Range; 2] = [calc(6, 1), calc(10, 1)];
+
 pub fn part2(input: &str) -> u64 {
-    solve(input, |s| {
-        (1..=s.len() / 2).filter(|i| s.len() % i == 0).any(|i| {
-            let (a, rest) = s.split_at(i);
-            rest.chunks_exact(i).all(|c| a == c)
-        })
-    })
+    let ints = list(sep2(u64, '-')).read(input);
+    solve(&ints, &HALVES) + solve(&ints, &NEW) - solve(&ints, &OVERLAP)
 }
