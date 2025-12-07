426a7d1db790e53c6ca46f7134374046de4e1fec
Dan Seminara <semi225599@gmail.com>
2025-12-07 12:24:54 -0500

    shorten

--- src/year2025/day07.rs
+++ src/year2025/day07.rs
@@ -1,30 +1,21 @@
-use crate::utils::*;
-
-fn dfs(grid: &mut Grid<u8>, pos: C<usize>) -> (u64, bool) {
-    match std::mem::replace(grid.get_mut(pos).unwrap_or(&mut 0), 0) {
-        0 => (0, false),
-        b'^' => {
-            let (l, r) = (dfs(grid, pos - C(0, 1)), dfs(grid, pos + C(0, 1)));
-            (u64::from(l.1 || r.1) + l.0 + r.0, true)
-        }
-        _ => (dfs(grid, pos + C(1, 0)).0, true),
-    }
+fn process(input: &str) -> (u32, u64) {
+    let (line, input) = input.split_once('\n').unwrap();
+    let mut splits = 0;
+    let mut top: Vec<_> = line.bytes().map(|b| u64::from(b == b'S')).collect();
+    input.lines().for_each(|line| {
+        line.bytes().enumerate().filter(|&(_, b)| b == b'^').for_each(|(i, _)| {
+            top[i - 1] += top[i];
+            top[i + 1] += top[i];
+            splits += u32::from(std::mem::take(&mut top[i]) > 0);
+        })
+    });
+    (splits, top.into_iter().sum())
 }
 
-pub fn part1(input: &str) -> u64 {
-    let mut grid: Grid<_> = input.bytes().collect();
-    let start = C(0, grid.cols / 2);
-    dfs(&mut grid, start).0
+pub fn part1(input: &str) -> u32 {
+    process(input).0
 }
 
 pub fn part2(input: &str) -> u64 {
-    let (line, input) = input.split_once('\n').unwrap();
-    let mut top = vec![1; line.len()];
-    input.lines().rev().for_each(|line| {
-        line.bytes()
-            .enumerate()
-            .filter(|&(_, b)| b == b'^')
-            .for_each(|(i, _)| top[i] = top[i - 1] + top[i + 1])
-    });
-    top[top.len() / 2]
+    process(input).1
 }
