93dd569f3740ac182a50df97bddce8e11edaca77
Dan Seminara <semi225599@gmail.com>
2025-12-12 03:14:38 -0500

    More correct square packing check

--- src/year2025/day12.rs
+++ src/year2025/day12.rs
@@ -1,12 +1,13 @@
 use crate::utils::parsers::*;
 
 pub fn part1(input: &str) -> usize {
-    // Just ignore the present areas, assume everything is a full 3x3.
     let mut space = separated_pair(sep2(u16, 'x'), ':', spaced(u16));
     input
         .lines()
         .filter_map(|line| space.parse(line).ok())
-        .filter(|((w, h), Sum(presents))| w * h >= 9 * presents)
+        // Pretend we're packing 3x3 squares, problem input either doesn't fit
+        // or has lots of empty space.
+        .filter(|((w, h), Sum(presents))| (w - w % 3) * (h - h % 3) >= 9 * presents)
         .count()
 }
 
